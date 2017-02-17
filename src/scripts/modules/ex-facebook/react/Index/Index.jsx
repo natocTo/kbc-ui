6139c06dd2802dd654305e79bf278814bb6d4972
Tomas Kacur <tomas.kacur@keboola.com>
2017-02-17 14:58:56 +0100

    feat(ex-fbs): show link to documentation

--- src/scripts/modules/ex-facebook/react/Index/Index.jsx
+++ src/scripts/modules/ex-facebook/react/Index/Index.jsx
@@ -129,17 +129,17 @@ export default function(COMPONENT_ID) {
                   You are about to run extraction.
                 </RunComponentButton>
               </li>
-              {/* <li>
-                  <a href={this.state.component.get('documentationUrl')} target="_blank">
+              <li>
+                <a href={this.state.component.get('documentationUrl')} target="_blank">
                   <i className="fa fa-question-circle fa-fw" /> Documentation
-                  </a>
-                  </li> */}
-          <li>
-            <DeleteConfigurationButton
-              componentId={COMPONENT_ID}
-              configId={this.state.configId}
-            />
-          </li>
+                </a>
+              </li>
+              <li>
+                <DeleteConfigurationButton
+                  componentId={COMPONENT_ID}
+                  configId={this.state.configId}
+                />
+              </li>
             </ul>
             <LatestJobs jobs={this.state.latestJobs} limit={3} />
             <LatestVersions
