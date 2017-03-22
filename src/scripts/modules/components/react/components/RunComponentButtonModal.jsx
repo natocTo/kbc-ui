8a068cae0a631283d116f99740f3a0259f01c496
Tomas Kacur <tomas.kacur@keboola.com>
2017-03-22 15:46:57 +0100

    refactor(runButton): remove use of modal trigger

--- src/scripts/modules/components/react/components/RunComponentButtonModal.jsx
+++ src/scripts/modules/components/react/components/RunComponentButtonModal.jsx
@@ -4,29 +4,31 @@ import {Modal, ButtonToolbar, Button} from 'react-bootstrap';
 module.exports = React.createClass({
 
   propTypes: {
-    onRequestHide: React.PropTypes.func.isRequired,
+    onHide: React.PropTypes.func.isRequired,
     onRequestRun: React.PropTypes.func.isRequired,
     title: React.PropTypes.string.isRequired,
-    body: React.PropTypes.string.isRequired
+    body: React.PropTypes.string.isRequired,
+    show: React.PropTypes.bool.isRequired
   },
 
   _handleRun: function() {
-    this.props.onRequestHide();
+    this.props.onHide();
     return this.props.onRequestRun();
   },
 
   render: function() {
     return (
       <Modal
+        show={this.props.show}
         title={this.props.title}
-        onRequestHide={this.props.onRequestHide}
+        onHide={this.props.onHide}
       >
         <div className="modal-body">
           {this.props.body}
         </div>
         <div className="modal-footer">
           <ButtonToolbar>
-            <Button bsStyle="link" onClick={this.props.onRequestHide}>Close</Button>
+            <Button bsStyle="link" onClick={this.props.onHide}>Close</Button>
             <Button bsStyle="primary" onClick={this._handleRun}>Run</Button>
           </ButtonToolbar>
         </div>
