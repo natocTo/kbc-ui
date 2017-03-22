48bb42f8816ef6d7238b7a0447a03c2d4ab0fef5
Tomas Kacur <tomas.kacur@keboola.com>
2017-03-22 17:28:37 +0100

    refactor(Confirm): remove use of ModalTrigger

--- src/scripts/react/common/Confirm.jsx
+++ src/scripts/react/common/Confirm.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import {ModalTrigger} from 'react-bootstrap';
 import ConfirmModal from './ConfirmModal';
 
 export default React.createClass({
@@ -9,26 +8,38 @@ export default React.createClass({
     onConfirm: React.PropTypes.func.isRequired,
     buttonLabel: React.PropTypes.string.isRequired,
     buttonType: React.PropTypes.string,
-    children: React.PropTypes.any
+    children: React.PropTypes.any,
+    childrenRootElement: React.PropTypes.any
   },
 
   getDefaultProps() {
     return {
-      buttonType: 'danger'
+      buttonType: 'danger',
+      childrenRootElement: React.DOM.span
     };
   },
 
+  getInitialState() {
+    return {
+      showModal: false
+    };
+  },
+
+  closeModal() {
+    this.setState({showModal: false});
+  },
+
+  onButtonClick(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    this.setState({showModal: true});
+  },
+
   render() {
-    return (
-      <ModalTrigger {...this.props} modal={<ConfirmModal {...this.props}/>}>
-        {React.cloneElement(React.Children.only(this.props.children), {
-          onClick: (e) => {
-            e.preventDefault();
-            e.stopPropagation();
-          }
-        })}
-      </ModalTrigger>
-    );
+    const modal = <ConfirmModal show={this.state.showModal} onHide={this.closeModal} {...this.props}/>;
+    return React.cloneElement(this.props.childrenRootElement,
+                              {onClick: this.onButtonClick},
+                              [this.props.children, modal]);
   }
 
 });
