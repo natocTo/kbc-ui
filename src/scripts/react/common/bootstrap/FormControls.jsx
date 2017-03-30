3f326ecd1bdb4b5347ee693dacc30a0890c7b402
Vladimír Kriška <vlado@keboola.com>
2017-03-30 16:39:53 +0200

    Add proxy module for FormControls.Static

--- src/scripts/react/common/bootstrap/FormControls.jsx
+++ src/scripts/react/common/bootstrap/FormControls.jsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { FormControls } from 'react-bootstrap';
+import { FormGroup, ControlLabel, FormControl } from 'react-bootstrap';
 
-export default React.createClass({
+const FormControls = {};
+
+FormControls.Static = React.createClass({
+
+  propTypes: {
+    label: React.PropTypes.node,
+    children: React.PropTypes.node,
+    bsSize: React.PropTypes.oneOf(['sm', 'small', 'lg', 'large']),
+    wrapperClassName: React.PropTypes.string,
+    labelClassName: React.PropTypes.string
+  },
+
   render() {
     return (
-      <FormControls {...this.props} />
+      <FormGroup bsSize={this.props.bsSize}>
+        <ControlLabel className={this.props.labelClassName}>
+          {this.props.label}
+        </ControlLabel>
+        <FormControl.Static className={this.props.wrapperClassName}>
+          {this.props.children}
+        </FormControl.Static>
+      </FormGroup>
     );
   }
+
 });
+
+export default FormControls;
