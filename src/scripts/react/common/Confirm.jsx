7ef75744a506df0c34a59744327b3226d17a0119
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-05-02 15:05:43 +0200

    fix(Confirm): missing key

--- src/scripts/react/common/Confirm.jsx
+++ src/scripts/react/common/Confirm.jsx
@@ -36,7 +36,7 @@ export default React.createClass({
   },
 
   render() {
-    const modal = <ConfirmModal show={this.state.showModal} onHide={this.closeModal} {...this.props}/>;
+    const modal = <ConfirmModal show={this.state.showModal} onHide={this.closeModal} {...this.props} key="modal"/>;
     return this.props.childrenRootElement({onClick: this.onButtonClick}, [this.props.children, modal]);
   }
 
