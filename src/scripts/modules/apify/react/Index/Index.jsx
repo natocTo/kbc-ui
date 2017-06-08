af3ba9cf6013657d811b6cbc73cf0f501ba3820b
Tomas Kacur <tomas.kacur@keboola.com>
2017-06-08 18:27:59 +0800

    (apify): init routes and index

---
+++ src/scripts/modules/apify/react/Index/Index.jsx
@@ -0,0 +1,103 @@
+import React from 'react';
+// import {Map} from 'immutable';
+
+// stores
+import storeProvisioning, {storeMixins} from '../../storeProvisioning';
+import ComponentStore from '../../../components/stores/ComponentsStore';
+import RoutesStore from '../../../../stores/RoutesStore';
+import LatestJobsStore from '../../../jobs/stores/LatestJobsStore';
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+import storageTablesStore from '../../../components/stores/StorageTablesStore';
+
+// actions
+import actionsProvisioning from '../../actionsProvisioning';
+
+// ui components
+import ComponentDescription from '../../../components/react/components/ComponentDescription';
+import ComponentMetadata from '../../../components/react/components/ComponentMetadata';
+import RunComponentButton from '../../../components/react/components/RunComponentButton';
+import DeleteConfigurationButton from '../../../components/react/components/DeleteConfigurationButton';
+import LatestJobs from '../../../components/react/components/SidebarJobs';
+import LatestVersions from '../../../components/react/components/SidebarVersionsWrapper';
+// import {Button} from 'react-bootstrap';
+
+const COMPONENT_ID = 'apify.apify';
+
+export default React.createClass({
+  mixins: [createStoreMixin(...storeMixins, LatestJobsStore)],
+
+  getStateFromStores() {
+    const configId = RoutesStore.getCurrentRouteParam('config');
+    const store = storeProvisioning(COMPONENT_ID, configId);
+    const actions = actionsProvisioning(COMPONENT_ID, configId);
+    const component = ComponentStore.getComponent(COMPONENT_ID);
+
+    return {
+      allTables: storageTablesStore.getAll(),
+      latestJobs: LatestJobsStore.getJobs(COMPONENT_ID, configId),
+      store: store,
+      actions: actions,
+      component: component,
+      configId: configId,
+      localState: store.getLocalState()
+    };
+  },
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="col-md-9 kbc-main-content">
+          <div className="row kbc-header">
+            <div className="col-sm-12">
+              <ComponentDescription
+                componentId={COMPONENT_ID}
+                configId={this.state.configId}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="col-md-3 kbc-main-sidebar">
+          <ComponentMetadata
+            configId={this.state.configId}
+            componentId={COMPONENT_ID}
+          />
+          <ul className="nav nav-stacked">
+            <li className={!!this.invalidToRun() ? 'disabled' : null}>
+              <RunComponentButton
+                title="Upload"
+                component={COMPONENT_ID}
+                mode="link"
+                runParams={this.runParams()}
+                disabled={!!this.invalidToRun()}
+                disabledReason={this.invalidToRun()}
+              >
+                You are about to upload data.
+              </RunComponentButton>
+            </li>
+            <li>
+              <DeleteConfigurationButton
+                componentId={COMPONENT_ID}
+                configId={this.state.configId}
+              />
+            </li>
+          </ul>
+          <LatestJobs jobs={this.state.latestJobs} limit={3} />
+          <LatestVersions
+            limit={3}
+            componentId={COMPONENT_ID}
+          />
+        </div>
+      </div>
+    );
+  },
+
+  invalidToRun() {
+    return '';
+  },
+
+  runParams() {
+    return () => ({config: this.state.configId});
+  }
+
+});
