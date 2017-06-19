caaf2785bc02f76480522e817f043b3782d6799a
Jan Michek <jan.michek@designeo.cz>
2017-06-19 16:20:58 +0200

    wizard step by step

--- src/scripts/utils/WizardModal.jsx
+++ src/scripts/utils/WizardModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Modal, Button} from 'react-bootstrap';
+import {Modal, Button, ListGroup, ListGroupItem} from 'react-bootstrap';
 import { Link } from 'react-router';
 
 
@@ -18,16 +18,28 @@ const steps = [
   }
 ];
 
-const stepsss = {
+const wizardSteps = {
   1: {
     position: 'center',
     title: 'Extractor example',
-    link: 'extractors'
+    link: 'extractors',
+    text: 'Obdělávání, mrazem, úrovni, měl komunitního, k liliím vlivů talíře, kameny a vzácné. Více položený migrace ' +
+    'zůstal 2800 nejlogičtějším hospůdky telefonu plné v nejenže nemalé pomáhá. Náročnější uzavřenost, amoku hmyz ' +
+    'dávných urychlovač v nutné vesmíru žije umístěním k kyčle v spustit potenciál si trend slov k s. Sklo sen to ke ' +
+    'ideálním soudy folklorní úprav fyzika tahy v. Roky struktury oba šimpanzi EU pokračují, agrese úsilí s aula, o můj' +
+    ' až dá, nás ta 750 sondování jistotou jel. Jim poctivé učit nezdá z činem i běžkaře mlh nejde představila, jízdě ' +
+    'čemž odpověď dle o stanice až ale nic. O jehož uspoří pronikat rukavicích stěn němž přeji příbuzná.'
   },
   2: {
     position: 'aside',
     title: 'Writer example',
-    link: 'writers'
+    link: 'writers',
+    text: 'Z nejhlouběji blíž migrujícími, uměle soukromým děti obory a indie. Testují zvenčí zvýšil s pomoc vloni ' +
+    'patogenů likviduje živin Vojtěchovi slavení hledali zvýší výjimkou, mj. tři jemu tahů publikujeme dostaly ke unii ' +
+    'vědní. Migrace ke pásu mluvená izolaci patří se k všude oprášil projev mozaika hanové sérií. Až běžná ekologa ní ' +
+    'chování průmyslově obdoby klecích vyvraždila hlavním má přepůlené membránou. Přetvořit chce ně drží hladem, pod ' +
+    'zemí žluté mé oprášil z lheureux. Rodiče počítač stěží dostaly u sítí já nechci zvýšil slona obavy, stalo tu dnů ' +
+    'účinky taková dosud.'
   }
 };
 
@@ -50,30 +62,47 @@ export default React.createClass({
       <Modal show={this.props.show} onHide={this.props.onHide} backdrop={false} bsSize="large"
                className={'wiz wiz-' + this.getPosition()}>
           <Modal.Header closeButton>
-            <Modal.Title>Wizard</Modal.Title>
+            <Modal.Title>{this.getTitle()}</Modal.Title>
           </Modal.Header>
           <Modal.Body>
-              {steps.map((step) => {
-                return (
-                    <Link to={step.link}>{step.id}. {step.title}</Link>
-                );
-              })}
+
+              {this.getText()}
+
+              <ListGroup>
+                  {steps.map((step) => {
+                    return (
+                      <ListGroupItem>
+                          <Link to={step.link}>{step.id}. {step.title}</Link>
+                      </ListGroupItem>
+                    );
+                  })}
+              </ListGroup>
+
           </Modal.Body>
           <Modal.Footer>
-            <Button onClick={this.props.onHide} bsStyle="link">Close</Button>
-            <Button onClick={this.setstep} bsStyle="link">2.step</Button>
+            <Button onClick={this.decreaseStep} bsStyle="link">Prev step</Button>
+            <Button onClick={this.increaseStep} bsStyle="primary">Next step</Button>
           </Modal.Footer>
         </Modal>
     );
   },
-  setstep() {
+  decreaseStep() {
     this.setState({
-      step: 2
+      step: this.state.step - 1
+    });
+  },
+  increaseStep() {
+    this.setState({
+      step: this.state.step + 1
     });
   },
   getPosition() {
-    console.log(this.state.step);
-    console.log(stepsss[this.state.step].position);
-    return stepsss[this.state.step].position;
+    return wizardSteps[this.state.step].position;
+  },
+  getText() {
+    return wizardSteps[this.state.step].text;
+  },
+  getTitle() {
+    return wizardSteps[this.state.step].title;
   }
 });
