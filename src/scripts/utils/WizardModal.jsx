b46cb2b67adb0edf8892d85ec03464b670dbe730
Jan Michek <jan.michek@designeo.cz>
2017-06-19 17:20:40 +0200

    ads isactive function

--- src/scripts/utils/WizardModal.jsx
+++ src/scripts/utils/WizardModal.jsx
@@ -7,47 +7,74 @@ const steps = [
   {
     id: 1,
     position: 'center',
-    title: 'Extractor example',
-    link: 'extractors'
-  },
-  {
-    id: 2,
-    position: 'aside',
-    title: 'Writer example',
-    link: 'writers'
-  }
-];
-
-const wizardSteps = {
-  1: {
-    position: 'center',
-    title: 'Extractor example',
-    link: 'extractors',
-    text: 'Obdělávání, mrazem, úrovni, měl komunitního, k liliím vlivů talíře, kameny a vzácné. Více položený migrace ' +
-    'zůstal 2800 nejlogičtějším hospůdky telefonu plné v nejenže nemalé pomáhá. Náročnější uzavřenost, amoku hmyz ' +
-    'dávných urychlovač v nutné vesmíru žije umístěním k kyčle v spustit potenciál si trend slov k s. Sklo sen to ke ' +
-    'ideálním soudy folklorní úprav fyzika tahy v. Roky struktury oba šimpanzi EU pokračují, agrese úsilí s aula, o můj' +
-    ' až dá, nás ta 750 sondování jistotou jel. Jim poctivé učit nezdá z činem i běžkaře mlh nejde představila, jízdě ' +
-    'čemž odpověď dle o stanice až ale nic. O jehož uspoří pronikat rukavicích stěn němž přeji příbuzná.'
-  },
-  2: {
-    position: 'aside',
-    title: 'Writer example',
-    link: 'writers',
+    title: 'Lesson 1 - Composition',
+    link: '',
     text: 'Z nejhlouběji blíž migrujícími, uměle soukromým děti obory a indie. Testují zvenčí zvýšil s pomoc vloni ' +
     'patogenů likviduje živin Vojtěchovi slavení hledali zvýší výjimkou, mj. tři jemu tahů publikujeme dostaly ke unii ' +
     'vědní. Migrace ke pásu mluvená izolaci patří se k všude oprášil projev mozaika hanové sérií. Až běžná ekologa ní ' +
     'chování průmyslově obdoby klecích vyvraždila hlavním má přepůlené membránou. Přetvořit chce ně drží hladem, pod ' +
     'zemí žluté mé oprášil z lheureux. Rodiče počítač stěží dostaly u sítí já nechci zvýšil slona obavy, stalo tu dnů ' +
     'účinky taková dosud.'
+
+  },
+  {
+    id: 2,
+    position: 'aside',
+    title: 'Extract data',
+    link: 'extractors',
+    text: 'Z nejhlouběji blíž migrujícími, uměle soukromým děti obory a indie. Testují zvenčí zvýšil s pomoc vloni ' +
+    'patogenů likviduje živin Vojtěchovi slavení hledali zvýší výjimkou, mj. tři jemu tahů publikujeme dostaly ke unii ' +
+    'vědní. Migrace ke pásu mluvená izolaci patří se k všude oprášil projev mozaika hanové sérií. Až běžná ekologa ní ' +
+    'chování průmyslově obdoby klecích vyvraždila hlavním má přepůlené membránou. Přetvořit chce ně drží hladem, pod ' +
+    'zemí žluté mé oprášil z lheureux. Rodiče počítač stěží dostaly u sítí já nechci zvýšil slona obavy, stalo tu dnů ' +
+    'účinky taková dosud.'
+
+  },
+  {
+    id: 3,
+    position: 'aside',
+    title: 'Store example',
+    link: 'storage',
+    text: 'Obdělávání, mrazem, úrovni, měl komunitního, k liliím vlivů talíře, kameny a vzácné. Více položený migrace ' +
+    'zůstal 2800 nejlogičtějším hospůdky telefonu plné v nejenže nemalé pomáhá. Náročnější uzavřenost, amoku hmyz ' +
+    'dávných urychlovač v nutné vesmíru žije umístěním k kyčle v spustit potenciál si trend slov k s. Sklo sen to ke ' +
+    'ideálním soudy folklorní úprav fyzika tahy v. Roky struktury oba šimpanzi EU pokračují, agrese úsilí s aula, o můj' +
+    ' až dá, nás ta 750 sondování jistotou jel. Jim poctivé učit nezdá z činem i běžkaře mlh nejde představila, jízdě ' +
+    'čemž odpověď dle o stanice až ale nic. O jehož uspoří pronikat rukavicích stěn němž přeji příbuzná.'
+
+  },
+  {
+    id: 4,
+    position: 'aside',
+    title: 'Write data',
+    link: 'writers',
+    text: 'Obdělávání, mrazem, úrovni, měl komunitního, k liliím vlivů talíře, kameny a vzácné. Více položený migrace ' +
+    'zůstal 2800 nejlogičtějším hospůdky telefonu plné v nejenže nemalé pomáhá. Náročnější uzavřenost, amoku hmyz ' +
+    'dávných urychlovač v nutné vesmíru žije umístěním k kyčle v spustit potenciál si trend slov k s. Sklo sen to ke ' +
+    'ideálním soudy folklorní úprav fyzika tahy v. Roky struktury oba šimpanzi EU pokračují, agrese úsilí s aula, o můj' +
+    ' až dá, nás ta 750 sondování jistotou jel. Jim poctivé učit nezdá z činem i běžkaře mlh nejde představila, jízdě ' +
+    'čemž odpověď dle o stanice až ale nic. O jehož uspoří pronikat rukavicích stěn němž přeji příbuzná.'
+
+  },
+  {
+    id: 5,
+    position: 'aside',
+    title: 'Check data',
+    link: 'jobs',
+    text: 'Obdělávání, mrazem, úrovni, měl komunitního, k liliím vlivů talíře, kameny a vzácné. Více položený migrace ' +
+    'zůstal 2800 nejlogičtějším hospůdky telefonu plné v nejenže nemalé pomáhá. Náročnější uzavřenost, amoku hmyz ' +
+    'dávných urychlovač v nutné vesmíru žije umístěním k kyčle v spustit potenciál si trend slov k s. Sklo sen to ke ' +
+    'ideálním soudy folklorní úprav fyzika tahy v. Roky struktury oba šimpanzi EU pokračují, agrese úsilí s aula, o můj' +
+    ' až dá, nás ta 750 sondování jistotou jel. Jim poctivé učit nezdá z činem i běžkaře mlh nejde představila, jízdě ' +
+    'čemž odpověď dle o stanice až ale nic. O jehož uspoří pronikat rukavicích stěn němž přeji příbuzná.'
+
   }
-};
+];
 
 export default React.createClass({
   displayName: 'WizardModal',
   propTypes: {
     onHide: React.PropTypes.func.isRequired,
-    setWizardStep: React.PropTypes.func.isRequired,
     show: React.PropTypes.bool.isRequired,
     position: React.PropTypes.string.isRequired,
     step: React.PropTypes.number.isRequired
@@ -67,11 +94,14 @@ export default React.createClass({
           <Modal.Body>
 
               {this.getText()}
-
               <ListGroup>
                   {steps.map((step) => {
+                    let isActive = 'active';
+                    if (this.state.step - 1 < step.id) {
+                      isActive = '';
+                    }
                     return (
-                      <ListGroupItem>
+                      <ListGroupItem className={isActive}>
                           <Link to={step.link}>{step.id}. {step.title}</Link>
                       </ListGroupItem>
                     );
@@ -97,12 +127,12 @@ export default React.createClass({
     });
   },
   getPosition() {
-    return wizardSteps[this.state.step].position;
+    return steps[this.state.step - 1].position;
   },
   getText() {
-    return wizardSteps[this.state.step].text;
+    return steps[this.state.step - 1].text;
   },
   getTitle() {
-    return wizardSteps[this.state.step].title;
+    return steps[this.state.step - 1].title;
   }
 });
