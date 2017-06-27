6a5297ffb6cf4783289cada3c0c1b9c176fb7346
Vladimír Kriška <vlado@keboola.com>
2017-06-27 20:59:45 +0200

    Init simple WizardStore

---
+++ src/scripts/utils/WizardStore.js
@@ -0,0 +1,49 @@
+import Dispatcher from '../Dispatcher';
+import StoreUtils from './StoreUtils';
+
+const ActionTypes = {
+  UPDATE_WIZARD_MODAL_STATE: 'UPDATE_WIZARD_MODAL_STATE'
+};
+
+let store = {
+  showLessonModal: false,
+  lessonNumber: 0
+};
+
+const WizardStore = StoreUtils.createStore({
+  getState() {
+    return store;
+  }
+});
+
+Dispatcher.register((payload) => {
+  let action = payload.action;
+
+  switch (action.type) {
+    case ActionTypes.UPDATE_WIZARD_MODAL_STATE:
+      store = {
+        showLessonModal: action.showLessonModal,
+        lessonNumber: action.lessonNumber
+      };
+      return WizardStore.emitChange();
+    default:
+  }
+});
+
+export default WizardStore;
+
+export const hideWizardModalFn = () => {
+  Dispatcher.handleViewAction({
+    type: ActionTypes.UPDATE_WIZARD_MODAL_STATE,
+    showLessonModal: false,
+    lessonNumber: 0
+  });
+};
+
+export const showWizardModalFn = (lessonNumber) => {
+  return Dispatcher.handleViewAction({
+    type: ActionTypes.UPDATE_WIZARD_MODAL_STATE,
+    showLessonModal: true,
+    lessonNumber: lessonNumber
+  });
+};
