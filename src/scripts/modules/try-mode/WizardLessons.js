391535613f2191a109b4e6aa7f1143870973a988
Jan Michek <jan.michek@designeo.cz>
2017-08-31 17:08:08 +0200

    Lesson 4 routing

--- src/scripts/modules/try-mode/WizardLessons.js
+++ src/scripts/modules/try-mode/WizardLessons.js
@@ -160,7 +160,7 @@ export default {
 - paste simple query \`SELECT * FROM cars;\` 
 - click on **Save**
 `,
-        nextStepDispatchAction: {type: INSTALLED_COMPONENTS_CONFIGDATA_SAVE_SUCCESS, componentId: 'keboola.ex-db-snowflake'},
+        // nextStepDispatchAction: {type: INSTALLED_COMPONENTS_CONFIGDATA_SAVE_SUCCESS, componentId: 'keboola.ex-db-snowflake'},
         'media': '',
         'mediaType': ''
       }, {
@@ -315,8 +315,70 @@ On \`cars\`.\`COUNTRY\` = \`population\`.\`COUNTRY\`;\`
         'title': 'Introduction',
         'link': 'home',
         'isNavigationVisible': true,
-        'text': 'žije umístěním k kyčle v spustit potenc indie. Testují zvenčí zvýšil s pomoc vloni ',
-        'media': 'http://keboola.asia/wp-content/uploads/2015/05/scema1-1024x724.png',
+        'text': 'Transformation results can be delivered to any analytics or business intelligence tool. In this lesson, we are going to generate a Tableau Data Extract and load it manually into Tableau Desktop for visualization using yet another Keboola Connection component – a Writer.',
+        'media': 'kbc_scheme_light_blue.svg',
+        'mediaType': 'img'
+      },
+      {
+        'id': 2,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Create Writer',
+        'link': 'writers',
+        'nextStepDispatchAction': {type: COMPONENTS_NEW_CONFIGURATION_SAVE_SUCCESS, componentId: 'tde-exporter'},
+        'isNavigationVisible': true,
+        'text': 'Let’s say we have Tableau Desktop installed. Now we need to find the Tableau writer in the Writers section.',
+        'markdown': `
+### TO DO:
+
+- Click on **+ New Writer**
+- Find **Tableau**. You can use the search feature to find it quickly.
+- Click on **More** and continue with **Create New Configuration**
+- Name the configuration and click on **Create**
+
+`,
+        'media': '',
+        'mediaType': ''
+      },
+      {
+        'id': 3,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Add Table',
+        nextStepDispatchAction: {type: JOB_LOAD_SUCCESS},
+        'markdown': `
+### TO DO:
+
+- Click on **+ Add Table**
+- Find **Tableau**. You can use the search feature to find it quickly.
+- Select  'in.c-tutrial.cars'
+- Select proper 'TDE Data Type' and Save this table
+- click Export
+
+`,
+        'isNavigationVisible': true,
+        'text': 'Then let’s add the table we want to send to Tableau. For each of its columns we also need to specify whether it contains text or a number.'
+      },
+
+      {
+        'id': 4,
+        'position': 'center',
+        'backdrop': true,
+        'title': 'Run Extracion',
+        'isNavigationVisible': false,
+        'text': 'The final step is to open the downloaded file in Tableau and create any reports we want. To see how to automate the whole process, continue to the next lesson - Project Automation. Learn more about Automation, or follow a hands-on tutorial on automation in our user documentation.',
+        'media': 'tbl.png',
+        'mediaType': 'img'
+      },
+      {
+        'id': 5,
+        'position': 'center',
+        'backdrop': true,
+        'title': 'Congratulations',
+        'isNavigationVisible': false,
+        'link': 'home',
+        'text': 'Congratulations! Lesson finished.',
+        'media': 'keboola-finished.svg',
         'mediaType': 'img'
       }
     ]
