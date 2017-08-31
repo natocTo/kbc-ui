bc9fed26752ef53c4cd41493b5b1654145dc1e88
Jan Michek <jan.michek@designeo.cz>
2017-08-31 17:58:38 +0200

    Lesson 5 routing

--- src/scripts/modules/try-mode/WizardLessons.js
+++ src/scripts/modules/try-mode/WizardLessons.js
@@ -1,10 +1,12 @@
 import ComponentsConstants from '../components/Constants';
 import TransformationsConstants from '../transformations/Constants';
 import JobsConstants from '../jobs/Constants';
+import OrchestrationsConstants from '../orchestrations/Constants';
 // import kbcConstants from '../../constants/KbcConstants';
 const {INSTALLED_COMPONENTS_CONFIGDATA_SAVE_SUCCESS, COMPONENTS_NEW_CONFIGURATION_SAVE_SUCCESS} = ComponentsConstants.ActionTypes;
 const {TRANSFORMATION_BUCKET_CREATE_SUCCESS, TRANSFORMATION_CREATE_SUCCESS} = TransformationsConstants.ActionTypes;
 const {JOB_LOAD_SUCCESS} = JobsConstants.ActionTypes;
+const {ORCHESTRATION_CREATE_SUCCESS, ORCHESTRATION_TASKS_EDIT_START, ORCHESTRATION_TASKS_SAVE_SUCCESS} = OrchestrationsConstants.ActionTypes;
 
 export default {
   '1': {
@@ -394,8 +396,75 @@ On \`cars\`.\`COUNTRY\` = \`population\`.\`COUNTRY\`;\`
         'title': 'Introduction',
         'link': 'home',
         'isNavigationVisible': true,
-        'text': 'žije umístěním k kyčle v spustit potenc indie. Testují zvenčí zvýšil s pomoc vloni ',
-        'media': 'http://keboola.asia/wp-content/uploads/2015/05/scema1-1024x724.png',
+        'text': 'To bring in the newest data available, the whole sequence of loading, transforming and writing must be done repeatedly. Keboola Connection Orchestrator does exactly that – automatically.',
+        'media': 'kbc_scheme_light_blue.svg',
+        'mediaType': 'img'
+      }, {
+        'id': 2,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Create Orchestration',
+        'link': 'orchestrations',
+        'nextStepDispatchAction': {type: ORCHESTRATION_CREATE_SUCCESS},
+        'isNavigationVisible': true,
+        'text': 'Add your first Orchestration by clicking **+ Add Orchestration** and naming it',
+        'media': '',
+        'mediaType': ''
+      }, {
+        'id': 3,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Orchestration Overview',
+        'isNavigationVisible': true,
+        'text': 'We only need to specify what tasks should be executed, in which order and when. We can also set up notifications that will be sent to us if something needs your attention. Start by click on ***Configure Tasks***',
+        'nextStepDispatchAction': {type: ORCHESTRATION_TASKS_EDIT_START},
+        'media': '',
+        'mediaType': ''
+      }, {
+        'id': 4,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Configure Task',
+        'isNavigationVisible': true,
+        'text': 'Lets compose new task in desired order from previous lessons. Start by clicking **+ Add Task***',
+        markdown: `
+### Order list:
+- Snowflake extractor
+- Transformation
+- Tableau writer 
+`,
+
+        'nextStepDispatchAction': {type: ORCHESTRATION_TASKS_SAVE_SUCCESS},
+        'media': '',
+        'mediaType': ''
+      }, {
+        'id': 5,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Automate Orchestration',
+        'isNavigationVisible': true,
+        'text': 'Then we choose how often, on what days and at what time we want the orchestration to run.',
+        'media': '',
+        'mediaType': ''
+      }, {
+        'id': 6,
+        'position': 'aside',
+        'backdrop': false,
+        'title': 'Setting Notifications',
+        'isNavigationVisible': true,
+        'text': 'Finally, we enter our email address to receive error notifications. From data extraction to data writing, we have set up the full pipeline. Any change to our Snowflake database tables will be automatically reflected in our Tableau results. Learn more about Automation, or follow a hands-on tutorial on automation in our user documentation.',
+        'media': '',
+        'mediaType': ''
+      },
+      {
+        'id': 7,
+        'position': 'center',
+        'backdrop': true,
+        'title': 'Congratulations',
+        'isNavigationVisible': false,
+        'link': 'home',
+        'text': 'Congratulations! Lesson finished.',
+        'media': 'keboola-finished.svg',
         'mediaType': 'img'
       }
     ]
