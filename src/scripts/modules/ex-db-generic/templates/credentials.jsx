a9bc0aa38da8014bd9e34329ec30b3d140e1a510
pivnicek <marc@keboola.com>
2017-09-12 18:26:53 +0200

    chore: move credentials template to jsx

---
+++ src/scripts/modules/ex-db-generic/templates/credentials.jsx
@@ -0,0 +1,93 @@
+// define as [labelValue, propName, type = 'text', isProtected = false, isRequired = false]
+
+const defaultFields = [
+  ['Host Name', 'host', 'text', false, true]
+  ['Port', 'port', 'number', false, true]
+  ['Username', 'user', 'text', false, true]
+  ['Password', '#password', 'password', true, true]
+  ['Database', 'database', 'text', false, true]
+];
+
+/*
+# impala fields:
+# host: impala
+# port: 21050
+# database: default
+# user: impala
+# password:
+# auth_mech: 0
+*/
+
+const firebirdFields = [
+  ['Database', 'dbname', 'text', false, true]
+  ['Username', 'user', 'text', false, true]
+  ['Password', '#password', 'password', true, true]
+];
+
+const oracleFields = [
+  ['Host Name', 'host', 'text', false, true]
+  ['Port', 'port', 'number', false, true]
+  ['Username', 'user', 'text', false, true]
+  ['Password', '#password', 'password', true, true]
+  ['Service Name/SID', 'database', 'text', false, true]
+];
+
+const snowflakeFields = [
+  ['Host Name', 'host', 'text', false, true]
+  ['Port', 'port', 'number', false, true]
+  ['Username', 'user', 'text', false, true]
+  ['Password', '#password', 'password', true, true]
+  ['Database', 'database', 'text', false, true]
+  ['Schema', 'schema', 'text', false, true]
+  ['Warehouse', 'warehouse', 'text', false, false]
+];
+
+const COMPONENTS_FIELDS = {
+  'keboola.ex-db-pgsql': defaultFields,
+  'keboola.ex-db-redshift': defaultFields,
+  'keboola.ex-db-db2': defaultFields,
+  'keboola.ex-db-firebird': firebirdFields,
+  'keboola.ex-db-impala': defaultFields,
+  'keboola.ex-db-oracle': oracleFields,
+  'keboola.ex-mongodb': defaultFields,
+  'keboola.ex-db-snowflake': snowflakeFields
+};
+
+export default react.createClass({
+  getFields(componentId) {
+    if (COMPONENTS_FIELDS[componentId]) {
+      return COMPONENTS_FIELDS[componentId];
+    } else {
+      return defaultFields;
+    }
+  },
+
+  // returns @array of properties that needs to be hashed
+  // should return only password property in most cases
+  getProtectedProperties(componentId) {
+    var result = [];
+    let fields = this.getFields(componentId);
+    for (f in fields) {
+      let isProtected = f[3];
+      let propName = f[1];
+      if (isProtected) {
+        result.push(propName);
+      }
+    }
+    return result;
+  },
+
+  // returns @array of properties that cannot be empty
+  getRequiredProperties(componentId) {
+    var result = [];
+    let fields = this.getFields(componentId);
+    for (f in fields) {
+      let isRequired = f[4];
+      let propName = f[1];
+      if (isRequired) {
+        result.push(propName);
+      }
+    }
+    return result;
+  }
+})
