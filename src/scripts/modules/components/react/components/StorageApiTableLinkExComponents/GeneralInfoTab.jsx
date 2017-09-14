add6570d1143677722110cfda7d56ec6dd87ad2c
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-09-14 16:01:15 +0200

    fix(TablePreview): null values

--- src/scripts/modules/components/react/components/StorageApiTableLinkExComponents/GeneralInfoTab.jsx
+++ src/scripts/modules/components/react/components/StorageApiTableLinkExComponents/GeneralInfoTab.jsx
@@ -54,8 +54,8 @@ export default React.createClass({
             {this.renderTableRow('Last Import', this.renderTimefromNow(table.get('lastImportDate')))}
             {this.renderTableRow('Last Change', this.renderTimefromNow(table.get('lastChangeDate')))}
 
-            {this.renderTableRow('Rows Count', table.get('rowsCount') + ' rows')}
-            {this.renderTableRow('Data Size', filesize(table.get('dataSizeBytes')))}
+            {this.renderTableRow('Rows Count', this.renderRowsCount(table.get('rowsCount')))}
+            {this.renderTableRow('Data Size', this.renderDataSize(table.get('dataSizeBytes')))}
             {this.renderTableRow('Indexed Column(s)', _.isEmpty(indexes) ? 'N/A' : indexes.join(', '))}
             {this.renderTableRow('Columns', table.get('columns').count() + ' columns: ' + table.get('columns').join(', '))}
           </tbody>
@@ -64,7 +64,24 @@ export default React.createClass({
     );
   },
 
+  renderRowsCount(value) {
+    if (value === null) {
+      return 'N/A';
+    }
+    return value + ' rows';
+  },
+
+  renderDataSize(value) {
+    if (value === null) {
+      return 'N/A';
+    }
+    return filesize(value);
+  },
+
   renderTimefromNow(value) {
+    if (value === null) {
+      return 'N/A';
+    }
     const fromNow = moment(value).fromNow();
     return (
       <span> {date.format(value)}
