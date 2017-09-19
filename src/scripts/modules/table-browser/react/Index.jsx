70db749aee86827383134da4bb3efcba16244438
Tomas Kacur <tomas.kacur@keboola.com>
2017-09-19 12:12:29 +0200

    feat(table-browser): load tables on page did mount(not in routes requiredata)

--- src/scripts/modules/table-browser/react/Index.jsx
+++ src/scripts/modules/table-browser/react/Index.jsx
@@ -105,7 +105,7 @@ export default React.createClass({
   },
 
   componentDidMount() {
-    setTimeout(() => this.onShow());
+    setTimeout(() => storageActions.loadTables().then(this.onShow));
   },
 
   componentWilUnmount() {
--- src/scripts/modules/table-browser/routes.js
+++ src/scripts/modules/table-browser/routes.js
@@ -1,11 +1,7 @@
 import Index from './react/Index';
-import storageActions from '../components/StorageActionCreators';
 
 export default {
   name: 'tables',
   path: 'tables/:tableId',
-  defaultRouteHandler: Index,
-  requireData: [
-    () => storageActions.loadTables()
-  ]
+  defaultRouteHandler: Index
 };
