74e15b2fafd704630f3c299873a32f49ddc06bdc
Tomas Kacur <tomas.kacur@keboola.com>
2017-09-20 12:00:30 +0200

    feat(table-browser): helper for create tables route

--- src/scripts/modules/table-browser/routes.js
+++ src/scripts/modules/table-browser/routes.js
@@ -1,6 +1,14 @@
 import Index from './react/Index';
 export const PATH_PREFIX = 'tables';
 
+export const createTablesRoute = (parentRouteName) => {
+  return {
+    name: `${parentRouteName}-tables`,
+    path: `${PATH_PREFIX}/:tableId`,
+    handler: Index
+  };
+};
+
 export default {
   name: 'tables',
   path: `${PATH_PREFIX}/:tableId`,
