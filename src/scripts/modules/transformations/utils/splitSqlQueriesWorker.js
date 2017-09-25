f123e0641d06fd92734061bd0e769a0306c72d7d
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-09-25 10:34:35 +0200

    fix(splitSqlQueriesWorker): remove doc comments

--- src/scripts/modules/transformations/utils/splitSqlQueriesWorker.js
+++ src/scripts/modules/transformations/utils/splitSqlQueriesWorker.js
@@ -1,8 +1,6 @@
 /**
  * Taken and modified from
  * http://stackoverflow.com/questions/4747808/split-mysql-queries-in-array-each-queries-separated-by/5610067#5610067
- * @param {string} queries SQL queries
- * @return {Object} List of queries
  */
 const regex = /\s*((?:'[^'\\]*(?:\\.[^'\\]*)*'|"[^"\\]*(?:\\.[^"\\]*)*"|\/\*[^*]*\*+(?:[^*\/][^*]*\*+)*\/|#.*|--.*|[^"';#])+(?:;|$))/g;
 const re = new RegExp(regex, 'g');
