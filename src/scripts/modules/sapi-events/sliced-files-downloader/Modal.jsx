f760fc4fcac310be3a5937705b1e32c7bf4a7300
Martin Halamíček <martin@halamicek.cz>
2017-10-20 13:05:46 +0200

    working package job execution

--- src/scripts/modules/sapi-events/sliced-files-downloader/Modal.jsx
+++ src/scripts/modules/sapi-events/sliced-files-downloader/Modal.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import {Modal} from 'react-bootstrap';
+import ConfirmButtons from '../../../react/common/ConfirmButtons';
 
 export default React.createClass({
   propTypes: {
     isModalOpen: React.PropTypes.bool,
     onModalHide: React.PropTypes.func,
-    file: React.PropTypes.object
+    onPrepareStart: React.PropTypes.func,
+    file: React.PropTypes.object,
+    isRunning: React.PropTypes.bool,
+    progress: React.PropTypes.string,
+    progressStatus: React.PropTypes.string
   },
 
   render() {
@@ -17,13 +22,48 @@ export default React.createClass({
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>File <strong>{this.props.file.get('name')}<strong> is sliced into multiple chunks.</p>
+          <p>File <strong>{this.props.file.get('name')}</strong> is sliced into multiple chunks.</p>
           <p>All chunks will be packed into <code>ZIP</code> file, and when the export is finished, you will be given link to download the file.</p>
         </Modal.Body>
         <Modal.Footer>
-          todo
+          {this.renderStatusBar()}
+          <ConfirmButtons
+            onCancel={this.props.onModalHide}
+            onSave={this.props.onPrepareStart}
+            saveLabel={'Prepare Package'}
+            cancelLabel={'Close'}
+            isSaving={this.props.isRunning}
+          />
         </Modal.Footer>
       </Modal>
     );
+  },
+
+  renderStatusBar() {
+    if (!this.props.isRunning && this.props.progressStatus !== 'danger') {
+      return null;
+    }
+    return (
+      <div className="pull-left" style={{padding: '6px 15px'}}>
+         <span className={'text-' + this.props.progressStatus}>
+           {this.renderStatusIcon(this.props.progressStatus)} {this.props.progress}
+           <span />
+         </span>
+      </div>
+    );
+  },
+
+  renderStatusIcon(progressStatus) {
+    if (progressStatus === 'success') {
+      return (
+        <span className="fa fa-check"/>
+      );
+    } else if (progressStatus === 'danger') {
+      return (
+        <span className="fa fa-times"/>
+      );
+    } else {
+      return null;
+    }
   }
 });
--- src/scripts/modules/sapi-events/sliced-files-downloader/ModalHandler.jsx
+++ src/scripts/modules/sapi-events/sliced-files-downloader/ModalHandler.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import Modal from './Modal';
 import { filesize } from '../../../utils/utils';
+import jobsApi from '../../jobs/JobsApi';
+import actionCreators from '../../components/InstalledComponentsActionCreators';
+
+const SLICED_FILES_DOWNLOADER_COMPONENT = 'keboola.sliced-files-downloader';
 
 export default React.createClass({
   propTypes: {
@@ -9,7 +13,12 @@ export default React.createClass({
 
   getInitialState() {
     return {
-      isModalOpen: false
+      isModalOpen: false,
+      isRunning: false,
+      jobId: null,
+      progress: null,
+      progressStatus: null,
+      isCreated: false
     };
   },
 
@@ -22,9 +31,90 @@ export default React.createClass({
           file={file}
           isModalOpen={this.state.isModalOpen}
           onModalHide={this.closeModal}
+          isRunning={this.state.isRunning}
+          onPrepareStart={this.startJob}
+          progress={this.state.progress}
+          progressStatus={this.state.progressStatus}
         />
       </a>
     );
+  },
+
+  startJob() {
+    this.setState({
+      isRunning: true,
+      jobId: null,
+      progress: 'Waiting for start...',
+      progressStatus: null
+    });
+    actionCreators.runComponent({
+      component: SLICED_FILES_DOWNLOADER_COMPONENT,
+      notify: false,
+      data: {
+        configData: {
+          storage: {
+            input: {
+              files: [
+                {
+                  query: `id:${this.props.file.get('id')}`
+                }
+              ]
+            }
+          }
+        }
+      }
+    }).then(this.handleJobReceive).catch((e) => {
+      this.setState({
+        isRunning: false
+      });
+      throw e;
+    });
+  },
+
+  handleJobReceive(job) {
+    if (!this.isMounted()) {
+      return;
+    }
+    if (job.isFinished) {
+      if (job.status === 'success') {
+        this.setState({
+          isRunning: false,
+          progress: 'Package was successfully loaded.',
+          progressStatus: 'success',
+          jobId: null,
+          isCreated: true
+        });
+      } else {
+        this.setState({
+          isRunning: false,
+          progress: 'Package create finished with an error.',
+          progressStatus: 'danger',
+          jobId: job.id,
+          isCreated: true
+        });
+      }
+    } else {
+      this.setState({
+        jobId: job.id,
+        progress: job.state === 'waiting' ? 'Waiting for start...' : 'Creating package...'
+      });
+      setTimeout(this.checkJobStatus, 5000);
+    }
+  },
+
+  checkJobStatus() {
+    if (!this.state.jobId) {
+      return;
+    }
+    jobsApi
+      .getJobDetail(this.state.jobId)
+      .then(this.handleJobReceive)
+      .catch((e) => {
+        this.setState({
+          isRunning: false
+        });
+        throw e;
+      });
   },
 
   openModal() {
