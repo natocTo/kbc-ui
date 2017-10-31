976b6f66a21381f6a77bea37d8ff06725784515d
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-10-31 15:22:13 +0100

    feat(Overview): deprecated transformations warning

--- src/scripts/modules/home/react/Deprecation.jsx
+++ src/scripts/modules/home/react/Deprecation.jsx
@@ -1,18 +1,90 @@
 import React, {PropTypes} from 'react';
 import StringUtils from '../../../utils/string';
 import ComponentDetailLink from '../../../react/common/ComponentDetailLink';
-import { List } from 'immutable';
+import Immutable from 'immutable';
+import TransformationBucketsStore from '../../transformations/stores/TransformationBucketsStore';
+import TransformationsStore from '../../transformations/stores/TransformationsStore';
+import ComponentConfigurationRowLink from '../../components/react/components/ComponentConfigurationRowLink';
 
 import './expiration.less';
 
 export default React.createClass({
   propTypes: {
-    components: PropTypes.object
+    components: PropTypes.object,
+    transformations: PropTypes.object
   },
 
   render() {
+    return (
+      <span>
+        {this.renderDeprecatedComponents()}
+        {this.renderDeprecatedTransformations()}
+      </span>
+    );
+  },
+
+  renderDeprecatedTransformations() {
+    if (!this.props.transformations) {
+      return null;
+    }
+    var deprecatedTransformationsInBuckets = new Immutable.Map();
+    this.props.transformations.forEach(function(bucket, index) {
+      const deprecatedTransformations = bucket.filter(function(transformation) {
+        return transformation.get('backend') === 'mysql';
+      });
+      if (deprecatedTransformations.count() > 0) {
+        deprecatedTransformationsInBuckets = deprecatedTransformationsInBuckets.set(index, deprecatedTransformations);
+      }
+    });
+
+    if (deprecatedTransformationsInBuckets.isEmpty()) {
+      return null;
+    }
+
+    return (
+        <div className="kbc-overview-component">
+          <div className="row kbc-header kbc-expiration kbc-deprecation">
+            <div className="alert alert-warning">
+              <h3>
+                Project contains deprecated MySQL transformations
+              </h3>
+
+              <div className="row">
+                {deprecatedTransformationsInBuckets.map(function(bucket, indexBucket) {
+                  return (
+                  <div className="col-md-6" key={indexBucket}>
+                    <h4>
+                      <span className={'kbc-transformation-icon'}/>
+                        {TransformationBucketsStore.get(indexBucket).get('name', indexBucket)}
+                    </h4>
+                    <ul>
+                    {bucket.map(function(transformation, indexTransformation) {
+                      return (
+                        <li key={indexTransformation}>
+                          <ComponentConfigurationRowLink
+                            componentId="transformation"
+                            configId={indexBucket}
+                            rowId={indexTransformation}
+                          >
+                            {TransformationsStore.getTransformation(indexBucket, indexTransformation).get('name', indexTransformation)}
+                          </ComponentConfigurationRowLink>
+                        </li>
+                      );
+                    })}
+                    </ul>
+               </div>
+                  );
+                })}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  },
+
+  renderDeprecatedComponents() {
     const deprecatedComponents = this.props.components.filter(function(component) {
-      return !!component.get('flags', List()).contains('deprecated');
+      return !!component.get('flags', Immutable.List()).contains('deprecated');
     });
 
     if (deprecatedComponents.isEmpty()) {
