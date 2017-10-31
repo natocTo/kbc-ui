35e2c527fa9b589fc01117d52574129f2fe98bf3
Erik Zigo <info@spoluprace.net>
2017-10-31 09:06:03 +0100

    ex-db: disable ssh tunnel for firebird

--- src/scripts/modules/ex-db-generic/templates/hasSshTunnel.js
+++ src/scripts/modules/ex-db-generic/templates/hasSshTunnel.js
@@ -1,7 +1,8 @@
 // by default all components have ssh tunnel, if some component dont
 // put it here:
 const componentsNotWithSsh = [
-  'keboola.ex-db-snowflake'
+  'keboola.ex-db-snowflake',
+  'keboola.ex-db-firebird'
 ];
 
 export default function(componentId) {
