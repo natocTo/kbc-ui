eece753341ad3663b9d845ee93d1fe72097a2d86
hhanova <hana@keboola.com>
2017-11-06 00:03:49 +0100

    2

--- src/scripts/modules/try-mode/lesson-templates/manipulatingData.js
+++ src/scripts/modules/try-mode/lesson-templates/manipulatingData.js
@@ -36,7 +36,7 @@ export default {
       'backdrop': false,
       'title': 'Create Transformation',
       'link': '',
-      'markdown': 'Now add a new transformation into the new bucket by clicking on **+ New Transformation**. Name the transformation *My transformation* and select MySQL as the backend &ndash; the engine running the transformation script.',
+      'markdown': 'Now add a new transformation into the new bucket by clicking on **+ New Transformation**. Name the transformation *My transformation* and select Snowflake as the backend &ndash; the engine running the transformation script.',
       'nextStepDispatchAction': {type: TRANSFORMATION_CREATE_SUCCESS},
       'media': '',
       'mediaType': ''
@@ -56,14 +56,11 @@ export default {
 - **Queries** &ndash; what will happen with the data in the transformation
 <br>
 <br>
-- Click on **+ New Input** and select \`in.c-tutorial.cars\` as Source from the drop down menu. Accept the suggested Destination. It is the name of the selected table inside the transformation. Press Create.
-- Repeat the steps for the second table called *Population*. Click on **+ New Input** and select \`in.c-tutorial.population\` as Source.
-- Click on **+ New Output** and set Destination to \`out.c-snowflake.transformed\`.
-- Click on **+ New Output** and set Destination at \`out.c-snowflake.transformed\`.
-- Paste this code to **Queries** \`CREATE TABLE transformed AS
-SELECT cars.*, population.POPULATION, (population.POPULATION / cars.CARS) AS PERSON_PER_CAR
-FROM cars JOIN population
-On cars.COUNTRY = population.COUNTRY\` and **Save** it.
+- Click on **+ New Input** and select \`in.c-tutorial.cars\` as the Source table from the drop down menu. Accept the suggested Destination. It is the name of the selected table inside the transformation. Press **Create Input**.
+- Repeat the steps for the second table. Click on **+ New Input** and select \`in.c-tutorial.population\` as the Source table.
+- Then click on **+ New Output** and enter *Transformed* as the Source table. This table does not exist yet. It will be created in the transformation. 
+- Set Destination to \`out.c-snowflake.transformed\`. This is the name the new table *Transformed* will have when put in Storage. Press **Create output**.
+- To create your output table, paste the following code to **Queries** \`CREATE TABLE "transformed" AS SELECT "cars".*, "population"."POPULATION", ("population"."POPULATION" / "cars"."CARS") AS "PERSON_PER_CAR" FROM "cars" JOIN "population" On "cars"."COUNTRY" = "population"."COUNTRY"\` and **Save** it.
 - Click on **Next Step**.
 
 `,
@@ -76,7 +73,7 @@ On cars.COUNTRY = population.COUNTRY\` and **Save** it.
       'backdrop': false,
       'title': 'Run Transformation',
       'link': '',
-      'markdown': 'Now when everything is ready, we can click on **Run transformation**. The specified tables will be taken from Storage and put in a transformation database where they will be changed by the queries. The result will be put in to Storage back again.',
+      'markdown': 'Now when everything is ready, you can click on **Run transformation**. <br/><br/> The specified tables will be taken from Storage and put in a transformation database where they will be changed by the queries. The result will be put in to Storage back again.',
       'nextStepDispatchAction': {type: JOB_LOAD_SUCCESS},
       'media': '',
       'mediaType': ''
@@ -87,8 +84,8 @@ On cars.COUNTRY = population.COUNTRY\` and **Save** it.
       'backdrop': false,
       'title': 'Check Jobs',
       'link': 'jobs',
-      'markdown': 'Here in **Jobs** you can see the status of your jobs. Our input tables were joined and the output table transformed was created. You can check the detail of Job if you want' +
-      '. When you are ready, hit **Next step**.',
+      'markdown': 'You can see the status of your jobs in the section **Jobs**. Your input tables were joined and the output table *Transformed* was created. You can check the details of each job here too.' +
+      '<br/><br/> When you are ready, hit **Next step**.',
       'media': '',
       'mediaType': ''
     }, {
@@ -98,7 +95,7 @@ On cars.COUNTRY = population.COUNTRY\` and **Save** it.
       'backdrop': false,
       'title': 'Check Storage',
       'link': 'storage',
-      'markdown': 'We can see what the final table looks like in this Data Sample in Storage. <br/><br/> To see how to write the output data from Keboola Connection, continue to next lesson - Visualizing Results. <br/><br/> Learn more about <a href="https://help.keboola.com/manipulation/" target="_blank">Data Manipulation</a>, or follow a hands-on tutorial on data transformation in our <a href="https://help.keboola.com/tutorial/load" target="_blank">user documentation</a>.',
+      'markdown': 'You can see what the final table looks like in this Data Sample in Storage. <br/><br/> To see how to write the output data from Keboola Connection, continue to the next lesson - Visualizing Results. <br/><br/> Learn more about <a href="https://help.keboola.com/manipulation/" target="_blank">Data Manipulation</a>, or follow a hands-on tutorial on data transformation in our <a href="https://help.keboola.com/tutorial/load" target="_blank">user documentation</a>.',
       'media': '',
       'mediaType': ''
     }
