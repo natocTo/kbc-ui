d94e51bdf8c36f618201ae93c9ee02b5f6a8b91a
hhanova <hana@keboola.com>
2017-11-05 23:19:33 +0100

    1

--- src/scripts/modules/try-mode/lesson-templates/loadingData.js
+++ src/scripts/modules/try-mode/lesson-templates/loadingData.js
@@ -15,7 +15,7 @@ export default {
       'backdrop': true,
       'title': 'Introduction',
       'link': 'home',
-      'markdown': 'As promised in Lesson 1, we are going to build a simple workflow analyzing data about car ownership stored two database tables. In this lesson, we will configure an Extractor that will access the prepared tables in a sample database. We will take the data in the tables and copy it into new tables created for this purpose in Keboola Connection Storage. <br/><br/> _Note: Your own projects won’t be affected by this in any way._',
+      'markdown': 'As promised in Lesson 1, you are about to build a simple workflow that analyzes data about car ownership stored in two database tables. <br/><br/> In this lesson you will start by configuring an extractor to access the prepared tables in a sample database, take the data in the tables and copy it into new tables created for this purpose in Keboola Connection Storage. <br/><br/> _Note: Your own projects won’t be affected by this in any way._',
       'media': 'kbc_scheme_light_blue-ext.svg',
       'mediaType': 'img'
     }, {
@@ -26,12 +26,12 @@ export default {
       'title': 'Create Extractor',
       'link': 'extractors',
       'nextStepDispatchAction': {type: COMPONENTS_NEW_CONFIGURATION_SAVE_SUCCESS, componentId: 'keboola.ex-db-snowflake'},
-      'markdown': 'Because the two data tables are stored in a Snowflake database, we’ll be using the Snowflake extractor. By configuring it, we’ll specify what data to extract and how.'
+      'markdown': 'Because the two data tables are stored in a Snowflake database, you’ll be using the Snowflake extractor. By configuring it, you’ll specify what data to bring from the external database to your project and how.'
                 + `
-- Click on <span class="btn btn-success btn-xs">+ New Extractor</span>
+- Click on <span class="btn btn-success btn-xs">+ New Extractor</span>.
 - Find **Snowflake**. You can use the search feature to find it quickly.
-- Click on <span class="btn btn-success btn-xs">More</span> and continue with <span class="btn btn-success btn-xs">+ New Configuration</span>
-- Name the configuration and click on <span class="btn btn-success btn-xs">Create Configuration</span>
+- Click on <span class="btn btn-success btn-xs">More</span> and continue with <span class="btn btn-success btn-xs">+ New Configuration</span>.
+- Name the configuration, e.g., *My database extractor*, and click on <span class="btn btn-success btn-xs">Create Configuration</span>.
 
 `,
       'media': '',
@@ -42,14 +42,14 @@ export default {
       'layout': 'content',
       'backdrop': false,
       'title': 'Setup Connection',
-      'markdown': 'After naming our new configuration of the extractor let’s configure it. Start by setting up the database connection.'
+      'markdown': 'Now you will configure the new extractor configuration. Start by providing a password and other credentials to access the source database where the data is stored.'
                 + `
-- Click on <span class="btn btn-success btn-xs">Setup Database Credentials</span>
-- Host to \`kebooladev.snowflakecomputing.com\`
-- Port to \`443\`
-- Username, Password, Database and Schema to \`HELP_TUTORIAL\`
-- Warehouse to \`DEV\`
-- Click on <span class="btn btn-success btn-xs">Save</span>
+- Click on <span class="btn btn-success btn-xs">Setup Database Credentials</span>.
+- Set Host to \`kebooladev.snowflakecomputing.com\`.
+- Set Port to \`443\`.
+- Set Username, Password, Database and Schema to \`HELP_TUTORIAL\`.
+- Set Warehouse to \`DEV\`.
+- Save the credentials by clickin on <span class="btn btn-success btn-xs">Save</span> in the upper right corner.
 `,
       'nextStepDispatchAction': {type: INSTALLED_COMPONENTS_CONFIGDATA_SAVE_SUCCESS, componentId: 'keboola.ex-db-snowflake'},
       'media': '',
@@ -61,13 +61,13 @@ export default {
       'backdrop': false,
       'title': 'Create SQL Query',
       'markdown':
-      'Each database query needs to have a name and an SQL command. The new **output table** that will be created in **Storage** has to be named here too. Let’s create the first query for extracting data about cars and save it.'
+      'Now it’s time to actually extract the data. You need to write two SQL queries, one for each table. Every database query needs to have a name, an SQL command specifying what to extract from the database, and a new **output table** where the data will be written in **Storage**. Start by creating the first query for extracting data about cars.'
       + `
-- Click on <span class="btn btn-success btn-xs">+ New Query</span>
-- Name the query \`cars\`
-- Set **Output table** \`in.c-tutorial.cars\`
-- Paste simple query \`SELECT * FROM cars;\`
-- Click on <span class="btn btn-success btn-xs">Create Query</span>
+- Click on <span class="btn btn-success btn-xs">+ New Query</span>.
+- Name the query \`cars\`.
+- Set **Output table** to \`in.c-tutorial.cars\`.
+- Paste this simple query: \`SELECT * FROM cars;\`
+- Click on <span class="btn btn-success btn-xs">Create Query</span>.
 
 `,
       'media': '',
@@ -79,15 +79,15 @@ export default {
       'position': 'aside',
       'layout': 'content',
       'backdrop': false,
-      'title': 'Create second SQL Query',
+      'title': 'Create Second SQL Query',
       'markdown':
-      'Similarly, we create a query to extract info about population.'
+      'Similarly, create a query to extract info about population.'
       + `
-- Click on <span class="btn btn-success btn-xs">+ New Query</span>
-- Name the query \`population\`
-- Set **Output table** \`in.c-tutorial.population\`
-- Paste simple query \`SELECT * FROM population;\`
-- Click on <span class="btn btn-success btn-xs">Create Query</span>
+- Click on <span class="btn btn-success btn-xs">+ New Query</span>.
+- Name the query \`population\`.
+- Set **Output table** to \`in.c-tutorial.population\`.
+- Paste this simple query: \`SELECT * FROM population;\`
+- Click on <span class="btn btn-success btn-xs">Create Query</span>.
 
 `,
       'media': '',
@@ -100,8 +100,8 @@ export default {
       'position': 'aside',
       'layout': 'content',
       'backdrop': false,
-      'title': 'Run Extracion',
-      'markdown': 'Now let’s click on <span class="btn btn-link btn-xs"> <i class="fa fa-play"></i> Run Extraction</span> to load the data from the two database tables into the new tables in **Storage**.',
+      'title': 'Run Extraction',
+      'markdown': 'Now click on <span class="btn btn-link btn-xs"> <i class="fa fa-play"></i> Run Extraction</span> to load the data from the two external database tables into the new tables in your project. You will find them in **Storage**.',
       'media': '',
       'mediaType': '',
       'nextStepDispatchAction': {type: JOB_LOAD_SUCCESS}
@@ -112,8 +112,8 @@ export default {
       'backdrop': false,
       'title': 'Stored Extraction',
       'link': 'storage',
-      'markdown': '**Storage** let you see that both tables have been successfully extracted from the Snowflake database and loaded into Keboola Connection. ' +
-      'To find out how you can work with the loaded data, continue to **Lesson 3 – Manipulating Data**. Learn more about <a href="https://help.keboola.com/overview/#extractors" target="_blank">Extractors</a>, or follow a hands-on tutorial on loading in our <a href="https://help.keboola.com/tutorial/load" target="_blank">user documentation</a>.',
+      'markdown': 'If you open your **Storage**, you can see that both tables have been successfully extracted from the Snowflake database and loaded into Keboola Connection. ' +
+      '<br/><br/> To find out how you can work with the loaded data, continue to **Lesson 3 – Manipulating Data**. <br/><br/> Learn more about <a href="https://help.keboola.com/overview/#extractors" target="_blank">Extractors</a>, or follow a hands-on tutorial on loading in our <a href="https://help.keboola.com/tutorial/load" target="_blank">user documentation</a>.',
       'media': '',
       'mediaType': ''
     }
