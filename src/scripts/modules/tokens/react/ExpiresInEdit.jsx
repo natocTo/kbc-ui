053346aed72ff7f6bcd3eec5b56c08c14bb0028f
Tomas Kacur <tomas.kacur@keboola.com>
2017-11-22 14:57:23 +0100

    tokens: setup token description and expires in period

---
+++ src/scripts/modules/tokens/react/ExpiresInEdit.jsx
@@ -0,0 +1,75 @@
+import React, {PropTypes} from 'react';
+import Select from 'react-select';
+
+const NEVER_EXPIRES = -2;
+const CUSTOM_VALUE = -1;
+export default React.createClass({
+
+  propTypes: {
+    value: PropTypes.number,
+    onChange: PropTypes.func.isRequired,
+    wrapperClassName: PropTypes.string
+  },
+
+  selectOptions: [
+    {label: 'Never',    value: NEVER_EXPIRES},
+    {label: '1 hour',   value: 1 * 3600},
+    {label: '24 hours', value: 24 * 3600},
+    {label: '48 hours', value: 48 * 3600},
+    {label: 'Custom',   value: CUSTOM_VALUE}
+  ],
+
+  getInitialState() {
+    return this.getStateFromProps(this.props);
+  },
+
+  componentWillReceiveProps(nextProps) {
+    this.setState(this.getStateFromProps(nextProps));
+  },
+
+  getStateFromProps(props) {
+    const hasSelectValue = props.value === null || this.selectOptions.reduce((memo, option) => memo || option.value === props.value, false);
+    const initSelectValue = props.value === null ? NEVER_EXPIRES : props.value;
+    const selectValue = this.state ? this.state.selectValue : initSelectValue;
+
+    return {
+      selectValue: hasSelectValue ? selectValue : CUSTOM_VALUE
+    };
+  },
+
+  render() {
+    return (
+      <div>
+        <Select
+          className="col-sm-4"
+          clearable={false}
+          searchable={false}
+          options={this.selectOptions}
+          value={this.state.selectValue}
+          onChange={this.handleSelectChange}
+        />
+        {this.state.selectValue === CUSTOM_VALUE &&
+         <span className="col-sm-5">
+           <input
+             type="number"
+             min="1"
+             value={this.props.value / 3600}
+             onChange={(e) => this.props.onChange(e.target.value * 3600)}
+             className="form-control"
+           />
+           <span className="help-block">
+             specified in hours
+           </span>
+         </span>
+         }
+      </div>
+    );
+  },
+
+  handleSelectChange({value}) {
+    if (value !== CUSTOM_VALUE) {
+      this.props.onChange(value === NEVER_EXPIRES ? null : value );
+    }
+    this.setState({selectValue: value});
+  }
+});
--- src/scripts/modules/tokens/react/ManageTokenModal.jsx
+++ src/scripts/modules/tokens/react/ManageTokenModal.jsx
@@ -1,6 +1,7 @@
 import React, {PropTypes} from 'react';
 import {Modal} from 'react-bootstrap';
 import ConfirmButtons from '../../../react/common/ConfirmButtons';
+import ExpiresInEdit from './ExpiresInEdit';
 
 export default React.createClass({
 
@@ -41,8 +42,22 @@ export default React.createClass({
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <div>
-            asdasd
+          <div className="form form-horizontal">
+            {this.renderFormGroup(
+               'Description',
+               <div className="col-sm-9">
+                 {this.renderDescriptionInput()}
+               </div>
+            )}
+            {this.props.isCreate && this.renderFormGroup(
+               'Expires In',
+               <ExpiresInEdit
+                 value={this.state.dirtyToken.get('expiresIn', null)}
+                 onChange={(value) => this.updateDirtyToken('expiresIn', value)}
+                 wrapperClassName="col-sm-9"
+               />
+            )}
+
           </div>
         </Modal.Body>
         <Modal.Footer>
@@ -55,6 +70,35 @@ export default React.createClass({
           />
         </Modal.Footer>
       </Modal>
+    );
+  },
+
+  updateDirtyToken(key, value) {
+    const {dirtyToken} = this.state;
+    this.setState({dirtyToken: dirtyToken.set(key, value)});
+  },
+
+  renderDescriptionInput() {
+    return (
+      <input
+        className="form-control"
+        type="text"
+        value={this.state.dirtyToken.get('description')}
+        onChange={(e) => this.updateDirtyToken('description', e.target.value)}
+      />
+    );
+  },
+
+  renderFormGroup(labelComponent, controlComponent) {
+    return (
+      <div className="form-group">
+        <label className="control-label col-sm-3">
+          {labelComponent}
+        </label>
+        <div className="col-sm-9">
+          {controlComponent}
+        </div>
+      </div>
     );
   },
 
