fcc064409ff2d75e3fdaea7b7b844b06b0a983aa
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-12-01 13:43:09 +0100

    feat: DeleteConfigRowButton (wip)

---
+++ src/scripts/modules/components/react/components/DeleteConfigRowButton.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+
+import Tooltip from '../../../../react/common/Tooltip';
+import {Loader} from 'kbc-react-components';
+
+const MODE_BUTTON = 'button', MODE_LINK = 'link';
+
+export default React.createClass({
+  displayName: 'DeleteConfigRowButton',
+  propTypes: {
+    onClick: React.PropTypes.func.isRequired,
+    isPending: React.PropTypes.bool.isRequired,
+    label: React.PropTypes.string,
+    buttonDisabled: React.PropTypes.bool,
+    mode: React.PropTypes.oneOf([MODE_BUTTON, MODE_LINK]),
+    tooltipPlacement: React.PropTypes.string
+  },
+
+  getDefaultProps() {
+    return {
+      buttonDisabled: false,
+      mode: MODE_BUTTON,
+      tooltipPlacement: 'top',
+      label: 'Move Row To Trash'
+    };
+  },
+
+  render() {
+    if (this.props.mode === MODE_BUTTON) {
+      return this.renderButton();
+    } else {
+      return this.renderLink();
+    }
+  },
+
+  renderButton() {
+    if (this.props.isPending) {
+      return (
+        <span className="btn btn-link">
+           <Loader className="fa-fw"/>
+         </span>
+      );
+    } else {
+      return (
+        <Tooltip placement={this.props.tooltipPlacement} tooltip={this.props.label}>
+          <button disabled={this.props.buttonDisabled}
+                  className="btn btn-link" onClick={this.props.onClick}>
+            <i className="kbc-icon-cup"/>
+          </button>
+        </Tooltip>
+      );
+    }
+  },
+
+  renderLink() {
+    // TODO
+    return;
+  }
+});
+
