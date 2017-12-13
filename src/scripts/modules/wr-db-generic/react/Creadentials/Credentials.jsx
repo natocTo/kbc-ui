307cfedbc40000e62b1da6f6b5448d440a33caa4
Erik Zigo <erik.zigo@keboola.com>
2017-12-13 19:48:27 +0100

    wip(wr-db-generic) credentials page

--- src/scripts/modules/wr-db-generic/react/Creadentials/Credentials.jsx
+++ src/scripts/modules/wr-db-generic/react/Creadentials/Credentials.jsx
@@ -1,9 +1,54 @@
 import React from 'react';
 
-const createClassTemplate = (componentId) => ({
-  render() {
-    return <div>WR DB {componentId} GENERIC CREDENTIALS </div>;
-  }
-});
+import SaveButtons from '../../../../react/common/SaveButtons';
 
-export default (componentId) => React.createClass(createClassTemplate(componentId));
+
+// import CredentialsForm from './CredentialsForm';
+
+import createStoreMixin from '../../../../react/mixins/createStoreMixin';
+
+import InstalledComponentsStore from '../../../components/stores/InstalledComponentsStore';
+// import WrDbStore from '../../../store';
+
+
+export default function(componentId, driver, isProvisioning) {
+  return React.createClass({
+    mixins: [createStoreMixin(InstalledComponentsStore)],
+
+    getStateFromStores() {
+      return {
+        //@TODO provisioning ma static state
+        isProvisioning: isProvisioning,
+        isSaving: false,
+        isChangedCredentials: true,
+        isValidCredentials: true
+      };
+    },
+
+    handleReset() {
+
+    },
+
+    handleSave() {
+
+    },
+
+    render() {
+      return (
+        <div className="container-fluid">
+          <div className="kbc-main-content">
+            <div className="kbc-inner-content-padding-fix text-right">
+              <SaveButtons
+                isSaving={this.state.isSaving}
+                isChanged={this.state.isChangedCredentials}
+                disabled={this.state.isSaving || !this.state.isValidCredentials}
+                onReset={this.handleReset}
+                onSave={this.handleSave}
+              />
+            </div>
+          </div>
+        </div>
+      );
+    }
+  });
+}
