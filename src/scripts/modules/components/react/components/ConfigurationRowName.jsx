da7966205de2879822d76327ad5080b0eda67a04
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2017-12-14 16:52:13 +0100

    feat: editable configuration row name

---
+++ src/scripts/modules/components/react/components/ConfigurationRowName.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import InlineEditTextInput from '../../../../react/common/InlineEditTextInput';
+import ConfigurationRowEditField from './ConfigurationRowEditField';
+
+module.exports = React.createClass({
+  displayName: 'ComponentName',
+  propTypes: {
+    componentId: React.PropTypes.string.isRequired,
+    configId: React.PropTypes.string.isRequired,
+    rowId: React.PropTypes.string.isRequired,
+    placeholder: React.PropTypes.string
+  },
+  getDefaultProps: function() {
+    return {
+      placeholder: 'My Configuration'
+    };
+  },
+  render: function() {
+    return React.createElement(ConfigurationRowEditField, {
+      componentId: this.props.componentId,
+      configId: this.props.configId,
+      rowId: this.props.rowId,
+      fieldName: 'name',
+      editElement: InlineEditTextInput,
+      placeholder: this.props.placeholder,
+      tooltipPlacement: 'bottom'
+    });
+  }
+});
