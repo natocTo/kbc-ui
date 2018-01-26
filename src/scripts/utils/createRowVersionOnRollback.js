844ea99d38f8cfbb1fe984b09568036bf96824df
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-01-26 12:23:54 +0100

    feat: config rows versions with rollback

---
+++ src/scripts/utils/createRowVersionOnRollback.js
@@ -0,0 +1,13 @@
+import VersionsActionCreators from '../modules/components/RowVersionsActionCreators';
+import InstalledComponentsActionCreators from '../modules/components/InstalledComponentsActionCreators';
+
+export default function(componentId, configId, rowId, versionId) {
+  return function() {
+    var reloadCallback = function(component, config) {
+      var promises = [];
+      promises.push(InstalledComponentsActionCreators.loadComponentConfigData(component, config));
+      return promises;
+    };
+    VersionsActionCreators.rollbackVersion(componentId, configId, rowId, versionId, reloadCallback);
+  };
+}
