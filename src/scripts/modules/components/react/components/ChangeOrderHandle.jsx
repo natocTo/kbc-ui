d9dd9f3307ee65bdb65fac419fdb76ae36bb5c1e
Jan Michek <jan.michek@designeo.cz>
2018-02-08 05:44:17 +0100

    changes drag icon

--- src/scripts/modules/components/react/components/ChangeOrderHandle.jsx
+++ src/scripts/modules/components/react/components/ChangeOrderHandle.jsx
@@ -46,9 +46,9 @@ export default React.createClass({
       return (<Loader className="fa-fw" style={{cursor: 'not-allowed'}}/>);
     }
     if (this.props.disabled) {
-      return (<span className="fa fa-bars fa-fw" style={{cursor: 'not-allowed', opacity: 0.65}}/>);
+      return (<span className="fa fa-ellipsis-v fa-fw" style={{cursor: 'not-allowed', opacity: 0.65}}/>);
     }
-    return ((<span className="fa fa-bars fa-fw drag-handle" style={{cursor: 'move'}}/>));
+    return ((<span className="fa fa-ellipsis-v fa-fw drag-handle" style={{cursor: 'move'}}/>));
   }
 });
 
