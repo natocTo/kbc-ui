3a837f3f75ff2f9a8919fc28552cecfacd803065
Jan Michek <jan.michek@designeo.cz>
2018-02-12 05:27:32 +0100

    text change to 'Output table'

--- src/scripts/modules/ex-pigeon/react/Index.jsx
+++ src/scripts/modules/ex-pigeon/react/Index.jsx
@@ -64,7 +64,7 @@ export default React.createClass({
     return (
       <FormGroup>
         <Col componentClass={ControlLabel} sm={4}>
-          Imported Table
+          Output Table
         </Col>
         <Col sm={8}>
           <InputGroup>
