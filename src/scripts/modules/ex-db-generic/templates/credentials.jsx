a0fd71de0d3617d5ce2cf6a5e1793a214a26ba65
pivnicek <marc@keboola.com>
2018-03-12 09:11:16 +0100

    update connection fields usages

--- src/scripts/modules/ex-db-generic/templates/credentials.jsx
+++ src/scripts/modules/ex-db-generic/templates/credentials.jsx
@@ -211,13 +211,10 @@ export function getFields(componentId) {
 // returns @array of properties that needs to be hashed
 // should return only password property in most cases
 export function getProtectedProperties(componentId) {
-  var result = [];
-  let fields = getFields(componentId);
-  fields.forEach(function(f) {
-    let isProtected = f[3];
-    let propName = f[1];
-    if (isProtected) {
-      result.push(propName);
+  let result = [];
+  getFields(componentId).forEach(function(f) {
+    if (f.protected) {
+      result.push(f.name);
     }
   });
   return result;
@@ -225,13 +222,10 @@ export function getProtectedProperties(componentId) {
 
 // returns @array of properties that cannot be empty
 export function getRequiredProperties(componentId) {
-  var result = [];
-  let fields = getFields(componentId);
-  fields.forEach(function(f) {
-    let isRequired = f[4];
-    let propName = f[1];
-    if (isRequired) {
-      result.push(propName);
+  let result = [];
+  getFields(componentId).forEach(function(f) {
+    if (f.required) {
+      result.push(f.name);
     }
   });
   return result;
