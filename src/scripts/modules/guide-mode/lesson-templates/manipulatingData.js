1cb82322e5466d46bb6976e607b9d993506b111c
Jan Michek <jan.michek@designeo.cz>
2018-03-20 06:43:43 +0100

    changes instruction

--- src/scripts/modules/guide-mode/lesson-templates/manipulatingData.js
+++ src/scripts/modules/guide-mode/lesson-templates/manipulatingData.js
@@ -61,7 +61,7 @@ export default {
       + `<br><br>
 **Input Mapping** — click <span class="btn btn-success btn-sm">+ New Input</span> and select *in.c-keboola-ex-db-snowflake.cars* as the Source. Accept the suggested destination. It’s the table's name inside the transformation. Click <span class="btn btn-success btn-sm">Create Input</span>. <br> Click again on <span class="btn btn-success btn-sm">+ New Input</span> and select *in.c-keboola-ex-db-snowflake.countries* as the Source. Accept the suggested destination. Click <span class="btn btn-success btn-sm">Create Input</span>.
 <br/><br/>
-**Output Mapping** — click <span class="btn btn-success btn-sm">+ New Output</span> and type \`cars_population\` as the Source. This table will be created in the transformation. Then set the Destination. Set \`out\` stage. Type in \`c-snowflake\` and create new option for bucket. Accept \`cars_population\` as a table. It’s a name for the new table *cars_population* that will be created in Storage. Click <span class="btn btn-success btn-sm">Create Output</span>.
+**Output Mapping** — click <span class="btn btn-success btn-sm">+ New Output</span> and type \`cars_population\` as the Source. This table will be created in the transformation. Then set the Destination. Set \`out\` stage. Accept \`c-guide-bucket\` as a bucket name. Accept \`cars_population\` as a table. It’s a name for the new table *cars_population* that will be created in Storage. Click <span class="btn btn-success btn-sm">Create Output</span>.
 <br/><br/>
 **Queries** — to create your output table, copy the following code and replace the default text with it: <br/> <pre><code>CREATE TABLE "cars_population" 
 AS SELECT 
