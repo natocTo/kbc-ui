739835c4b569790c550f125050aa85378f357496
Tomas Kacur <tomas.kacur@keboola.com>
2018-04-03 13:14:44 +0200

    Revert "Revert "get rid of static state in generic components ui""
    This reverts commit 9ae0edb86c5a507b314a1c6d715da0d934b51726.

--- src/scripts/modules/components/react/components/Configuration.jsx
+++ src/scripts/modules/components/react/components/Configuration.jsx
@@ -1,5 +1,4 @@
 import React, {PropTypes} from 'react';
-import Static from './ConfigurationStatic';
 import Edit from './ConfigurationEdit';
 import Immutable from 'immutable';
 import Markdown from '../../../../react/common/Markdown';
@@ -10,16 +9,14 @@ require('codemirror/mode/javascript/javascript');
 export default React.createClass({
   propTypes: {
     data: PropTypes.string.isRequired,
-    isEditing: PropTypes.bool.isRequired,
+    isChanged: PropTypes.bool.isRequired,
     isSaving: PropTypes.bool.isRequired,
-    onEditStart: PropTypes.func.isRequired,
     onEditCancel: PropTypes.func.isRequired,
     onEditChange: PropTypes.func.isRequired,
     onEditSubmit: PropTypes.func.isRequired,
     isValid: PropTypes.bool.isRequired,
     supportsEncryption: PropTypes.bool.isRequired,
     headerText: PropTypes.string,
-    editLabel: PropTypes.string,
     saveLabel: PropTypes.string,
     help: PropTypes.node,
     schema: PropTypes.object,
@@ -32,7 +29,6 @@ export default React.createClass({
     return {
       headerText: 'Configuration',
       help: null,
-      editLabel: 'Edit configuration',
       saveLabel: 'Save configuration',
       schema: Immutable.Map(),
       showDocumentationLink: true
@@ -46,7 +42,7 @@ export default React.createClass({
         {this.props.help}
         {this.renderDocumentationUrl()}
         {this.renderHelp()}
-        {this.scripts()}
+        {this.renderEditor()}
       </div>
     );
   },
@@ -82,27 +78,13 @@ export default React.createClass({
     return null;
   },
 
-  scripts() {
-    if (this.props.isEditing) {
-      return this.renderEditor();
-    } else {
-      return (
-        <Static
-          data={this.props.data}
-          schema={this.props.schema}
-          onEditStart={this.props.onEditStart}
-          editLabel={this.props.editLabel}
-          />
-      );
-    }
-  },
-
   renderEditor() {
     return (
       <Edit
         data={this.props.data}
         schema={this.props.schema}
         isSaving={this.props.isSaving}
+        isChanged={this.props.isChanged}
         onSave={this.props.onEditSubmit}
         onChange={this.props.onEditChange}
         onCancel={this.props.onEditCancel}
