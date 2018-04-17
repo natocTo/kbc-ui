0ba5cef4c2c0e590a6da56a57b91c84dfbd426e1
Tomas Kacur <tomas.kacur@keboola.com>
2018-04-17 19:41:32 +0200

    gdwrv3: wrap columns editor section spec with helper functions

--- src/scripts/modules/gooddata-writer-v3/adapters/tableColumnsEditor.js
+++ src/scripts/modules/gooddata-writer-v3/adapters/tableColumnsEditor.js
@@ -1,30 +1,35 @@
-import {Map, fromJS} from 'immutable';
+import {fromJS} from 'immutable';
 
-export default {
-  createConfiguration(localState) {
-    const columns = localState.get('columns').filter(c => c.get('type') !== 'IGNORE');
-    const result = columns.reduce((memo, column) => memo.set(column.get('id'), column.delete('id')), Map());
-    return localState.set('columns', result);
-  },
+function prepareTableColumnsEditorSection(
+  onLoadColumns,
+  onSaveColumns,
+  initColumnFn,
+  columnsKey = 'columns',
+  isColumnIgnored = column => column.get('type') === 'IGNORE',
+  columnIdKey = 'id') {
+  return {
+    createConfiguration(localState) {
+      const localStateColumnsToSave = localState.get(columnsKey).filter(c => !isColumnIgnored(c));
+      return localState.set(columnsKey, onSaveColumns(localStateColumnsToSave));
+    },
 
-  parseConfiguration(configuration, tables) {
-    const configColumns = configuration.get('columns', Map());
-    const tableId = configuration.get('tableId');
-    const storageTable = tables.get(tableId);
-    const storageTableColumns = storageTable.get('columns');
-    const deletedColumns = configColumns
-      .filter((val, id)  => !storageTableColumns.find(c => c === id))
-      .keySeq().toList();
-    const allColumns = storageTableColumns.concat(deletedColumns);
-    const defaultColumn = Map({type: 'IGNORE'});
-    const columnsList = allColumns.map(c => configColumns
-      .get(c, defaultColumn.set('title', c)).set('id', c)
-    );
-    return fromJS({columns: columnsList, tableId: tableId});
-  },
+    parseConfiguration(configuration, tables) {
+      const configColumns = onLoadColumns(configuration.get(columnsKey));
+      const tableId = configuration.get('tableId');
+      const storageTable = tables.get(tableId);
+      const storageTableColumns = storageTable.get('columns');
+      const deletedColumns = configColumns
+        .filter(column =>
+          !storageTableColumns.find(c => c === column.get(columnIdKey)));
+      const allColumns = storageTableColumns.concat(deletedColumns);
+      const columnsList = allColumns.map(c => configColumns.get(c, initColumnFn(c)));
+      return fromJS({[columnsKey]: columnsList, tableId: tableId});
+    },
 
-  createEmptyConfiguration() {
-    return fromJS({columns: []});
-  }
+    createEmptyConfiguration() {
+      return fromJS({[columnsKey]: []});
+    }
+  };
+}
 
-};
+export default  prepareTableColumnsEditorSection;
