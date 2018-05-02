581955c4570b589f67f32fc8bc48a0ad552d1d2c
Tomas Kacur <tomas.kacur@keboola.com>
2018-05-02 16:59:28 +0200

    jobs: show all imported/exported tables via PanelWithDetails

--- src/scripts/modules/jobs/react/pages/job-detail/TablesList.jsx
+++ src/scripts/modules/jobs/react/pages/job-detail/TablesList.jsx
@@ -2,8 +2,12 @@ import React from 'react';
 
 import duration from '../../../../../utils/duration';
 import TableLinkEx from '../../../../components/react/components/StorageApiTableLinkEx';
+import {PanelWithDetails} from '@keboola/indigo-ui';
+
+const limit = 10;
 
 export default React.createClass({
+
   propTypes: {
     tables: React.PropTypes.object.isRequired,
     allTablesIds: React.PropTypes.object.isRequired
@@ -13,42 +17,68 @@ export default React.createClass({
     return duration(Math.round(durationSeconds));
   },
 
-  rows() {
-    const limit = 10;
-    let rows = this.props.tables
-                   .get('tables')
-                   .toSeq()
-                   .slice(0, limit)
-                   .map((table) => {
-                     return (
-                       <li key={table.get('id')}>
-                         <TableLinkEx
-                           moreTables={this.props.allTablesIds}
-                           tableId={table.get('id')}>
-                           {table.get('id')} <span className="text-muted">{this.duration(table.get('durationTotalSeconds'))}</span>
-                         </TableLinkEx>
-                       </li>
-                     );
-                   })
-                   .toArray();
+  renderSlicedItems(start, count) {
+    return this.props.tables
+               .get('tables')
+               .toSeq()
+               .slice(start, count)
+               .map(this.renderListItem)
+               .toArray();
+  },
 
+  renderListItem(table) {
+    return (
+      <li key={table.get('id')}>
+        <TableLinkEx
+          moreTables={this.props.allTablesIds}
+          tableId={table.get('id')}>
+          {table.get('id')}
+          <span className="text-muted">
+            {' ' + this.duration(table.get('durationTotalSeconds'))}
+          </span>
+        </TableLinkEx>
+      </li>
+    );
+  },
+
+  renderWithPanel() {
+    const tablesCount = this.props.tables.get('tables').count();
+    const headerRows = this.renderSlicedItems(0, limit);
+    const panelContentRows = this.renderSlicedItems(limit, tablesCount);
+    const labelOpen = tablesCount === 100 ? `More than ${tablesCount - limit} others.` : `${tablesCount - limit} others`;
+    return (
+      <span>
+        <ul>
+          {headerRows}
+        </ul>
+        <PanelWithDetails
+          labelOpen={labelOpen}
+          labelCollapse="Show less">
+          <ul>
+            {panelContentRows}
+          </ul>
+        </PanelWithDetails>
+      </span>
+    );
+  },
+
+  rows() {
     const tablesCount = this.props.tables.get('tables').count();
     if (tablesCount > limit) {
-      const message = tablesCount === 100 ? `More than ${tablesCount - limit} others.` : `${tablesCount - limit} others`;
-      rows.push(
-        <li key="more"><span>{message}</span></li>
+      return this.renderWithPanel();
+    } else {
+      return (
+        <ul>
+          {this.renderSlicedItems(0, tablesCount)}
+        </ul>
       );
     }
-
-    return rows;
   },
 
   render() {
-    const rows = this.rows();
-    if (rows.length) {
-      return (
-        <ul>{this.rows()}</ul>
-      );
+    const tablesCount = this.props.tables.get('tables').count();
+    if (tablesCount > 0) {
+      return this.rows();
     } else {
       return (
         <div className="text-muted">No tables.</div>
