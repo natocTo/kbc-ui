aa21110685869b3bc0702e71971d9f39bc0d61f5
pivnicek <marc@keboola.com>
2018-05-09 11:24:18 +0200

    (MigrateToRowsButton) remove displayName and require componentId

--- src/scripts/modules/ex-db-generic/react/components/MigrateToRowsButton.jsx
+++ src/scripts/modules/ex-db-generic/react/components/MigrateToRowsButton.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 export default React.createClass({
-  displayName: 'MigrateToRowsButton',
   propTypes: {
-    componentId: React.PropTypes.string,
+    componentId: React.PropTypes.string.isRequired,
     configId: React.PropTypes.string.isRequired,
     actionsProvisioning: React.PropTypes.object.isRequired
   },
