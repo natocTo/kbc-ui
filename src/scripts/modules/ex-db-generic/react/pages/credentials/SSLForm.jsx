f7df81164efbc5e11446e412e70974f52cfa8b81
pivnicek <marc@keboola.com>
2018-05-10 17:01:45 +0200

    remove static form from SSLForm

--- src/scripts/modules/ex-db-generic/react/pages/credentials/SSLForm.jsx
+++ src/scripts/modules/ex-db-generic/react/pages/credentials/SSLForm.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 
 import Textarea from 'react-textarea-autosize';
-import {Input, FormControls} from './../../../../../react/common/KbcBootstrap';
+import {Input} from './../../../../../react/common/KbcBootstrap';
 
-import {NewLineToBr, Check} from '@keboola/indigo-ui';
 import {HelpBlock} from 'react-bootstrap';
 
 const helpUrl = 'https://help.keboola.com/extractors/database/sqldb/#mysql-encryption';
-const StaticText = FormControls.Static;
 
 export default React.createClass({
   displayName: 'SSLForm',
@@ -36,72 +34,47 @@ export default React.createClass({
     return this.props.data.get('enabled', false);
   },
 
-  renderEnableCheckbox(propName) {
-    if (this.props.isEditing) {
-      return (
-        <Input
-          disabled={!this.props.isEditing || this.props.disabledCheckbox}
-          type="checkbox"
-          label={<span>Encrypted (SSL) connection {this.renderHelp()}</span>}
-          wrapperClassName="col-xs-8 col-xs-offset-4"
-          checked={this.isEnabled()}
-          onChange={this.handleToggle.bind(this, propName)}
-        />
-      );
-    } else {
-      return (
-        <StaticText
-          label={<span>Encrypted connection (SSL) <small>{this.renderHelp()}</small></span>}
-          labelClassName="col-xs-4"
-          wrapperClassName="col-xs-8">
-          <Check isChecked={this.isEnabled()} />
-        </StaticText>
-      );
-    }
-  },
-
-  createStaticControl(labelValue, propName) {
+  renderSslCheckbox(propName) {
     return (
-      <StaticText
-        label={labelValue}
-        labelClassName="col-xs-4"
-        wrapperClassName="col-xs-8">
-        {this.isEnabled() ? <NewLineToBr text={this.props.data.get(propName)}/> : ''}
-      </StaticText>
+      <Input
+        disabled={!this.props.isEditing || this.props.disabledCheckbox}
+        type="checkbox"
+        label={<span>Encrypted (SSL) connection {this.renderHelp()}</span>}
+        wrapperClassName="col-xs-8 col-xs-offset-4"
+        checked={this.isEnabled()}
+        onChange={this.handleToggle.bind(this, propName)}
+      />
     );
   },
 
   createInput(labelValue, propName, help = null) {
-    if (this.props.isEditing) {
-      return (
-        <div>
-          <div className="form-group">
-            <label className="control-label col-xs-4">
-              {labelValue}
-            </label>
-            <div className="col-xs-8">
-              <Textarea
-                label={labelValue}
-                type="textarea"
-                value={this.props.data.get(propName)}
-                onChange={this.handleChange.bind(this, propName)}
-                className="form-control"
-                minRows={4}
-              />
-              {help && <HelpBlock>{help}</HelpBlock>}
-            </div>
+    return (
+      <div>
+        <div className="form-group">
+          <label className="control-label col-xs-4">
+            {labelValue}
+          </label>
+          <div className="col-xs-8">
+            <Textarea
+              label={labelValue}
+              type="textarea"
+              disabled={!this.props.isEditing}
+              value={this.props.data.get(propName)}
+              onChange={this.handleChange.bind(this, propName)}
+              className="form-control"
+              minRows={4}
+            />
+            {help && <HelpBlock>{help}</HelpBlock>}
           </div>
         </div>
-      );
-    } else {
-      return this.createStaticControl(labelValue, propName);
-    }
+      </div>
+    );
   },
 
   render() {
     return (
       <div>
-        {this.renderEnableCheckbox('enabled')}
+        {this.renderSslCheckbox('enabled')}
         {this.isEnabled() ?
           <div>
             {this.createInput('SSL Client Certificate (client-cert.pem)', 'cert')}
