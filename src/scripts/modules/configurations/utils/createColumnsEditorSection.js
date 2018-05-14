dd7ae9867919f4baccb20309d55bedd6d48044ff
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-05-14 11:59:34 +0200

    fix(createColumnsEditorSection): better variable names, indentation

--- src/scripts/modules/configurations/utils/createColumnsEditorSection.js
+++ src/scripts/modules/configurations/utils/createColumnsEditorSection.js
@@ -3,19 +3,21 @@ import StorageTableColumnsEditor from '../react/components/StorageTableColumnsEd
 
 export default (params) => {
   if (!params) return null;
-  const {onLoadColumns, // remap columns object/array stored in config to localstate array columnsObject => columnsArray
-         onSaveColumns, // remap columns array from localstate to config representation (columnsArray) => columnsObject
-         initColumnFn, // initial value of column given by its name (columnName) => columnObject
-         columnsKey = 'columns', // property in config/localstate representing columns object/array
-         matchColumnKey = 'id', // used for matching columns objects with storage table columns by its name (columnName, columnObject) => true/false
-         columnsMappings = [], // array of object containing render and title property
-         isComplete = () => true, // is representation complete?
-         isColumnIgnored = column => column.get('type') === 'IGNORE'} = params; // if ignored then won't be saved to input mapping columns property of configuration object
+  const {
+    onLoadColumns, // remap columns object/array stored in config to localstate array columnsObject => columnsArray
+    onSaveColumns, // remap columns array from localstate to config representation (columnsArray) => columnsObject
+    initColumnFn, // initial value of column given by its name (columnName) => columnObject
+    columnsKey = 'columns', // property in config/localstate representing columns object/array
+    matchColumnKey = 'id', // used for matching columns objects with storage table columns by its name (columnName, columnObject) => true/false
+    columnsMappings = [], // array of object containing render and title property
+    isComplete = () => true, // is representation complete?
+    isColumnIgnored = column => column.get('type') === 'IGNORE'
+  } = params; // if ignored then won't be saved to input mapping columns property of configuration object
 
   return fromJS({
     onSave(localState) {
       const localStateColumnsToSave = localState.get(columnsKey)
-                                                .filter(c => !isColumnIgnored(c));
+        .filter(column => !isColumnIgnored(column));
       return localState
         .set(columnsKey, onSaveColumns(localStateColumnsToSave))
         .delete('columnsMappings');
@@ -27,10 +29,16 @@ export default (params) => {
       const storageTable = tables.get(tableId);
       const storageTableColumns = storageTable.get('columns');
       const deletedColumns = configColumns
-        .filter(column =>
-          !storageTableColumns.find(c => c === column.get(matchColumnKey)));
-      const allColumns = storageTableColumns.concat(deletedColumns);
-      const columnsList = allColumns.map(c => configColumns.find(cc => cc.get(matchColumnKey) === c, null, initColumnFn(c)));
+        .filter(configColumn =>
+          !storageTableColumns.find(tableColumn => tableColumn === configColumn.get(matchColumnKey)));
+      const allTableColumns = storageTableColumns.concat(deletedColumns);
+      const columnsList = allTableColumns.map(
+        tableColumn => configColumns.find(
+          configColumn => configColumn.get(matchColumnKey) === tableColumn,
+          null,
+          initColumnFn(tableColumn)
+        )
+      );
       return fromJS({[columnsKey]: columnsList, tableId: tableId, columnsMappings});
     },
 
