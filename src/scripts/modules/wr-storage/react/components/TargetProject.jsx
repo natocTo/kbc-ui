d67f75abed263d0850a3332e36d37e3f5ddc4aa7
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-05-17 21:06:16 +0200

    fix(wr-storage): TargetProject horizontal form

--- src/scripts/modules/wr-storage/react/components/TargetProject.jsx
+++ src/scripts/modules/wr-storage/react/components/TargetProject.jsx
@@ -1,7 +1,7 @@
 import React, { PropTypes } from 'react';
 import immutableMixin from 'react-immutable-render-mixin';
 // import { Input } from './../../../../react/common/KbcBootstrap';
-import {FormControl, FormGroup, ControlLabel, HelpBlock} from 'react-bootstrap';
+import {FormControl, FormGroup, ControlLabel, HelpBlock, Form, Col} from 'react-bootstrap';
 
 export default React.createClass({
   mixins: [immutableMixin],
@@ -18,36 +18,45 @@ export default React.createClass({
   render() {
     const props = this.props;
     return (
-      <form>
+      <Form horizontal>
         <FormGroup>
-          <ControlLabel>Project Region</ControlLabel>
-          <FormControl
-            componentClass="select"
-            placeholder="select"
-            value={this.props.value.url}
-            onChange={function(e) {
-              props.onChange({url: e.target.value});
-            }}
-            disabled={this.props.disabled}
-          >
-            <option value="" disabled>Select region</option>
-            <option value="https://connection.keboola.com/">US</option>
-            <option value="https://connection.eu-central-1.keboola.com/">EU</option>
-          </FormControl>
+          <Col componentClass={ControlLabel} sm={4}>
+            Project Region
+          </Col>
+          <Col sm={8}>
+            <FormControl
+              componentClass="select"
+              placeholder="select"
+              value={this.props.value.url}
+              onChange={function(e) {
+                props.onChange({url: e.target.value});
+              }}
+              disabled={this.props.disabled}
+            >
+              <option value="" disabled>Select region</option>
+              <option value="https://connection.keboola.com/">US</option>
+              <option value="https://connection.eu-central-1.keboola.com/">EU</option>
+            </FormControl>
+          </Col>
         </FormGroup>
         <FormGroup>
-          <ControlLabel>Storage API Token</ControlLabel>
-          <FormControl
-            type="password"
-            value={this.props.value.token}
-            onChange={function(e) {
-              props.onChange({token: e.target.value});
-            }}
-            disabled={this.props.disabled}
-            />
+          <Col componentClass={ControlLabel} sm={4}>
+            Storage API Token
+          </Col>
+          <Col sm={8}>
+            <FormControl
+              type="password"
+              value={this.props.value.token}
+              onChange={function(e) {
+                props.onChange({token: e.target.value});
+              }}
+              disabled={this.props.disabled}
+              />
             <HelpBlock>Use token with permissions limited only to write to a single target bucket.</HelpBlock>
+          </Col>
+
         </FormGroup>
-      </form>
+      </Form>
     );
   }
 });
