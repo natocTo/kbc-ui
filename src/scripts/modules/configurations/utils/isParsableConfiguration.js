394f0651431778573e65f2de049e6e8cd5c2a8b9
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-05-17 21:31:54 +0200

    feat(configurations): isParsableConfiguration accepts incomplete configurations

--- src/scripts/modules/configurations/utils/isParsableConfiguration.js
+++ src/scripts/modules/configurations/utils/isParsableConfiguration.js
@@ -6,5 +6,11 @@ export default function(configuration, parseFunction, createFunction) {
   }
   const parsed = parseFunction(configuration);
   const reconstructed = createFunction(parsed);
-  return Immutable.is(configuration, reconstructed);
+  /*
+    incomplete configurations (eg. missing certain keys)
+    that do not affect the config itself should be considered parsable
+    merging is is used for backward compatibility when a property is added
+  */
+  const merged = configuration.mergeDeep(reconstructed);
+  return Immutable.is(merged, reconstructed);
 }
