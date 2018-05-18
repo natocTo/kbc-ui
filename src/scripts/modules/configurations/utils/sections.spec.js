29cc7d93871bf1ae083f8eba7a918223090c2b38
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-05-18 09:49:03 +0200

    feat(configuration): demonstrate sections configuration parsing bug

---
+++ src/scripts/modules/configurations/utils/sections.spec.js
@@ -0,0 +1,48 @@
+import assert from 'assert';
+import Immutable from 'immutable';
+import sections from './sections';
+
+describe('sections', function() {
+  describe('demonstrate bug, invalidValue gets passed through', function() {
+    const createBySectionsFn = sections.makeCreateFn(
+      null,
+      Immutable.fromJS([
+        {
+          onSave: function(localState) {
+            return Immutable.fromJS({
+              parameters: {
+                key: localState.get('key', '')
+              }
+            });
+          }
+        }
+      ])
+    );
+    const parseBySectionsFn = sections.makeParseFn(
+      null,
+      Immutable.fromJS([
+        {
+          onLoad: function(configuration) {
+            return Immutable.fromJS({
+              key: configuration.getIn(['parameters', 'key'], '')
+            });
+          }
+        }
+      ])
+    );
+    const configuration = Immutable.fromJS(
+      {
+        parameters: {
+          key: 'value',
+          invalidKey: 'invalidValue'
+        }
+      }
+    );
+    const expected = {
+      parameters: {
+        key: 'value'
+      }
+    };
+    assert.equal(expected, createBySectionsFn(parseBySectionsFn(configuration)).toJS());
+  });
+});
