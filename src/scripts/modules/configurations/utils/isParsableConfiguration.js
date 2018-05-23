9632bfa60e3f5dde81bee99199effd359fdb1d1c
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-05-23 11:59:37 +0200

    Revert "fix(configurations): use custom normalize function instead of a blind deepmerge in isParsableConfiguration"
    This reverts commit 4723724c2eb0b90a3d596b860a6d619159ab52c3.

--- src/scripts/modules/configurations/utils/isParsableConfiguration.js
+++ src/scripts/modules/configurations/utils/isParsableConfiguration.js
@@ -1,21 +1,16 @@
 var Immutable = require('immutable');
 
-export default function(configuration, parseFunction, createFunction, normalizeFunction) {
+export default function(configuration, parseFunction, createFunction) {
   if (configuration.isEmpty()) {
     return true;
   }
   const parsed = parseFunction(configuration);
   const reconstructed = createFunction(parsed);
   /*
-    normalize function takes the original configuration and fills in optional properties
-    so that the config can be visually displayed when a new configuration parameter in the ui
-    is introduced
-    see the test
-   */
-  if (normalizeFunction) {
-    const normalized = normalizeFunction(configuration);
-    return Immutable.is(normalized, reconstructed);
-  } else {
-    return Immutable.is(configuration, reconstructed);
-  }
+    incomplete configurations (eg. missing certain keys)
+    that do not affect the config itself should be considered parsable
+    merging is is used for backward compatibility when a property is added
+  */
+  const merged = configuration.mergeDeep(reconstructed);
+  return Immutable.is(merged, reconstructed);
 }
