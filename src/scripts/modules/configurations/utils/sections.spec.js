31a1d772724d41a3cb2c230d791abb1a4cb23988
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-05-23 12:34:42 +0200

    fix(configurations): fixed sections tests

--- src/scripts/modules/configurations/utils/sections.spec.js
+++ src/scripts/modules/configurations/utils/sections.spec.js
@@ -3,7 +3,119 @@ import Immutable from 'immutable';
 import sections from './sections';
 
 describe('sections', function() {
-  describe('demonstrate bug, invalidKey gets passed through', function() {
+  it('should simple merge', function() {
+    const createBySectionsFn = sections.makeCreateFn(
+      null,
+      Immutable.fromJS([
+        {
+          onSave: function(localState) {
+            return Immutable.fromJS({
+              parameters: {
+                key1: localState.get('key1', '')
+              }
+            });
+          }
+        },
+        {
+          onSave: function(localState) {
+            return Immutable.fromJS({
+              parameters: {
+                key2: localState.get('key2', '')
+              }
+            });
+          }
+        }
+      ])
+    );
+    const parseBySectionsFn = sections.makeParseFn(
+      null,
+      Immutable.fromJS([
+        {
+          onLoad: function(configuration) {
+            return Immutable.fromJS({
+              key1: configuration.getIn(['parameters', 'key1'], '')
+            });
+          }
+        },
+        {
+          onLoad: function(configuration) {
+            return Immutable.fromJS({
+              key2: configuration.getIn(['parameters', 'key2'], '')
+            });
+          }
+        }
+
+      ])
+    );
+    const configuration = Immutable.fromJS({
+      parameters: {
+        key1: 'value1',
+        key2: 'value2'
+      }
+    });
+    assert.deepEqual(configuration.toJS(), createBySectionsFn(parseBySectionsFn(configuration)).toJS());
+  });
+
+  it('should deep merge', function() {
+    const createBySectionsFn = sections.makeCreateFn(
+      null,
+      Immutable.fromJS([
+        {
+          onSave: function(localState) {
+            return Immutable.fromJS({
+              parameters: {
+                nested: {
+                  key1: localState.get('key1', '')
+                }
+              }
+            });
+          }
+        },
+        {
+          onSave: function(localState) {
+            return Immutable.fromJS({
+              parameters: {
+                nested: {
+                  key2: localState.get('key2', '')
+                }
+              }
+            });
+          }
+        }
+      ])
+    );
+    const parseBySectionsFn = sections.makeParseFn(
+      null,
+      Immutable.fromJS([
+        {
+          onLoad: function(configuration) {
+            return Immutable.fromJS({
+              key1: configuration.getIn(['parameters', 'nested', 'key1'], '')
+            });
+          }
+        },
+        {
+          onLoad: function(configuration) {
+            return Immutable.fromJS({
+              key2: configuration.getIn(['parameters', 'nested', 'key2'], '')
+            });
+          }
+        }
+
+      ])
+    );
+    const configuration = Immutable.fromJS({
+      parameters: {
+        nested: {
+          key1: 'value1',
+          key2: 'value2'
+        }
+      }
+    });
+    assert.deepEqual(configuration.toJS(), createBySectionsFn(parseBySectionsFn(configuration)).toJS());
+  });
+
+  it('silently demonstrate bug, invalidKey gets passed through', function() {
     const createBySectionsFn = sections.makeCreateFn(
       null,
       Immutable.fromJS([
@@ -30,19 +142,27 @@ describe('sections', function() {
         }
       ])
     );
-    const configuration = Immutable.fromJS(
-      {
-        parameters: {
-          key: 'value',
-          invalidKey: 'invalidValue'
-        }
+    const configuration = Immutable.fromJS({
+      parameters: {
+        key: 'value',
+        invalidKey: 'invalidValue'
       }
-    );
+    });
+
+    /*
+    in fact this should be, waiting for fix
     const expected = {
       parameters: {
         key: 'value'
       }
     };
-    assert.equal(expected, createBySectionsFn(parseBySectionsFn(configuration)).toJS());
+    */
+    const expected = {
+      parameters: {
+        key: 'value',
+        invalidKey: 'invalidValue'
+      }
+    };
+    assert.deepEqual(expected, createBySectionsFn(parseBySectionsFn(configuration)).toJS());
   });
 });
