14f809ac38cf777ff58bdbf0625d55e75784f511
Vladimír Kriška <vlado@keboola.com>
2018-06-04 19:48:29 +0200

    Add descriptionPlain to badge object

--- src/scripts/react/common/componentHelpers.js
+++ src/scripts/react/common/componentHelpers.js
@@ -16,24 +16,26 @@ const getComponentBadges = (component) => {
 
   let badges = [];
 
-  if (!flags.contains('3rdParty')) {
-    badges.push({
-      title: <span><Icon.Keboola className="badge-component-item-responsibility-icon" /> Keboola</span>,
-      description: `Support for this ${componentType} is provided by Keboola.`,
-      key: 'responsibility'
-    });
-  }
   if (flags.contains('3rdParty')) {
     badges.push({
       title: <span>3<sup>rd</sup> party</span>,
       description: `This is a third-party ${componentType} supported by its vendor.`,
+      descriptionPlain: `This is a third-party ${componentType} supported by its vendor.`,
       key: '3rdParty'
+    });
+  } else {
+    badges.push({
+      title: <span><Icon.Keboola className="badge-component-item-responsibility-icon" /> Keboola</span>,
+      description: `Support for this ${componentType} is provided by Keboola.`,
+      descriptionPlain: `Support for this ${componentType} is provided by Keboola.`,
+      key: 'responsibility'
     });
   }
   if (flags.contains('excludeFromNewList')) {
     badges.push({
       title: 'Alpha',
       description: `This ${componentType} is private.`,
+      descriptionPlain: `This ${componentType} is private.`,
       key: 'excludeFromNewList'
     });
   }
@@ -41,6 +43,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-cloud-download fa-fw"/> IN</span>,
       description: `This ${componentType} retrieves data from outside sources.`,
+      descriptionPlain: `This ${componentType} retrieves data from outside sources.`,
       key: 'dataIn'
     });
   }
@@ -48,6 +51,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-cloud-upload fa-fw"/> OUT</span>,
       description: `This ${componentType} sends data outside of Keboola Connection.`,
+      descriptionPlain: `This ${componentType} sends data outside of Keboola Connection.`,
       key: 'dataOut'
     });
   }
@@ -55,6 +59,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: 'Beta',
       description: `The ${componentType} is public, but it's in beta stage.`,
+      descriptionPlain: `The ${componentType} is public, but it's in beta stage.`,
       key: 'appInfo.beta'
     });
   }
@@ -62,6 +67,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-dollar fa-fw"/></span>,
       description: `There is an extra charge to use this ${componentType}.`,
+      descriptionPlain: `There is an extra charge to use this ${componentType}.`,
       key: 'fee'
     });
   }
@@ -69,6 +75,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-database fa-fw"/></span>,
       description: `A Redshift backend is required to use this ${componentType}.`,
+      descriptionPlain: `A Redshift backend is required to use this ${componentType}.`,
       key: 'redshift'
     });
   }
@@ -76,6 +83,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-key fa-fw"/></span>,
       description: `This ${componentType} will have full access to the project including all its data.`,
+      descriptionPlain: `This ${componentType} will have full access to the project including all its data.`,
       key: 'fullAccess'
     });
   }
@@ -83,6 +91,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-exclamation-triangle fa-fw"/><i className="fa fa-clock-o fa-fw"/></span>,
       description: `This ${componentType} is deprecated.`,
+      descriptionPlain: `This ${componentType} is deprecated.`,
       key: 'deprecated'
     });
   }
@@ -90,6 +99,7 @@ const getComponentBadges = (component) => {
     badges.push({
       title: <span><i className="fa fa-file-text-o fa-fw"/></span>,
       description: <span>You agree to the <ExternalLink href={component.getIn(['data', 'vendor', 'licenseUrl'])}>vendor's license agreement</ExternalLink>.</span>,
+      descriptionPlain: 'You agree to the vendor\'s license agreement.',
       key: 'license'
     });
   }
