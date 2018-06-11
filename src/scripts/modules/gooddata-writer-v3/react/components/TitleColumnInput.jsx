ce6cc79f6f94824ababd34e374ca58af2b95cfbe
Tomas Kacur <tomas.kacur@keboola.com>
2018-06-11 18:33:52 +0200

    wrgdv3: show title column only if needed

--- src/scripts/modules/gooddata-writer-v3/react/components/TitleColumnInput.jsx
+++ src/scripts/modules/gooddata-writer-v3/react/components/TitleColumnInput.jsx
@@ -1,5 +1,6 @@
 import React, {PropTypes} from 'react';
 import {FormControl} from 'react-bootstrap';
+import ColumnDefinition from '../../helpers/ColumnDefinition';
 
 export default React.createClass({
   propTypes: {
@@ -10,11 +11,16 @@ export default React.createClass({
 
   render() {
     const {disabled, onChange, column} = this.props;
-    return (<FormControl
-              type="text"
-              disabled={disabled}
-              onChange={e => onChange({...column, title: e.target.value})}
-              value={column.title}
-    />);
+    const {fields} = ColumnDefinition(column);
+    if (fields.title.show) {
+      return (<FormControl
+                type="text"
+                disabled={disabled}
+                onChange={e => onChange({...column, title: e.target.value})}
+                value={column.title}
+      />);
+    } else {
+      return null;
+    }
   }
 });
