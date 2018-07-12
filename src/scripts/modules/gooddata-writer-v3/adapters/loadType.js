1f0e4c06ae7c4fc3273e703471aad5c7a917cbf3
Tomas Kacur <tomas.kacur@keboola.com>
2018-07-12 21:34:14 +0200

    wrgdv3: better load type section with fact grain

--- src/scripts/modules/gooddata-writer-v3/adapters/loadType.js
+++ src/scripts/modules/gooddata-writer-v3/adapters/loadType.js
@@ -1,26 +1,47 @@
-import {Map, fromJS} from 'immutable';
+import {Map, fromJS, List} from 'immutable';
 import {parseParameters, createConfigParameters, createInputMapping, parseInputMapping} from '../helpers/rowParametersTable';
+import {Types} from '../helpers/Constants';
+
+const GRAIN_TYPES = [
+  Types.REFERENCE,
+  Types.ATTRIBUTE,
+  Types.DATE
+];
+
 
 const createConfiguration = (localState) => {
-  const incrementalLoad = localState.get('incrementalLoad', 0);
-  const storage = incrementalLoad > 0 ? createInputMapping(Map({changed_since: incrementalLoad})) : Map();
-  const localStateToSave = localState.remove('incrementalLoad');
-  return storage.merge(createConfigParameters(localStateToSave));
+  const incremental = localState.get('incremental', false);
+  const changedSince = localState.get('changedSince', '');
+  const storage =  createInputMapping(Map({changed_since: changedSince, incremental}));
+  const grain = localState.get('grain') || List();
+  const parameters = createConfigParameters(fromJS({
+    grain: grain.count() > 0 && !localState.get('hasConnectionPoint') ? grain : null,
+    tableId: localState.get('tableId')
+  }));
+  return storage.merge(parameters);
 };
 
 export default {
   createConfiguration: createConfiguration,
-
   parseConfiguration(configuration) {
     const parametersTable = parseParameters(configuration);
-    return Map({
+    const configColumns = parametersTable.get('columns', Map());
+    const grainColumns = configColumns
+      .filter(column => GRAIN_TYPES.includes(column.get('type')))
+      .map((column, columnId) => columnId)
+      .valueSeq().toList();
+    const hasConnectionPoint = !!configColumns.find(column => column.get('type') === Types.CONNECTION_POINT);
+    return fromJS({
+      grainColumns,
+      hasConnectionPoint,
       tableId: parametersTable.get('tableId'),
-      incrementalLoad: parseInputMapping(configuration).get('changed_since', 0),
-      grain: parametersTable.get('grain', null)
+      incremental: parseInputMapping(configuration).get('incremental', false),
+      changedSince: parseInputMapping(configuration).get('changed_since', ''),
+      grain: parametersTable.get('grain') || List()
     });
   },
 
   createEmptyConfiguration(name) {
-    return createConfiguration(fromJS({tableId: name, grain: null, incrementalLoad: 0}));
+    return createConfiguration(fromJS({tableId: name, grain: null}));
   }
 };
--- src/scripts/modules/gooddata-writer-v3/react/components/LoadTypeSection.jsx
+++ src/scripts/modules/gooddata-writer-v3/react/components/LoadTypeSection.jsx
@@ -1,11 +1,16 @@
 import React, {PropTypes} from 'react';
-import {Radio, HelpBlock, FormGroup, ControlLabel} from 'react-bootstrap';
+import {Form, Radio, HelpBlock, FormGroup, ControlLabel, Col} from 'react-bootstrap';
+import Select from 'react-select';
+import ChangedSinceInput from '../../../../react/common/ChangedSinceInput';
 
 export default React.createClass({
   propTypes: {
     value: PropTypes.shape({
-      incrementalLoad: PropTypes.number.isRequired,
-      grain: PropTypes.object
+      incremental: PropTypes.bool.isRequired,
+      changedSince: PropTypes.string.isRequired,
+      grainColumns: PropTypes.array.isRequired,
+      hasConnectionPoint: PropTypes.bool.isRequired,
+      grain: PropTypes.array
     }),
     onChange: PropTypes.func.isRequired,
     disabled: PropTypes.bool.isRequired
@@ -13,41 +18,72 @@ export default React.createClass({
 
   render() {
     const {value, onChange, disabled} = this.props;
+    const isIncremental = value.incremental;
     return (
-      <form>
+      <Form horizontal>
         <FormGroup>
-          <ControlLabel>Load type</ControlLabel>
-          <Radio
-            type="radio"
-            title="Full Load"
-            disabled={disabled}
-            onChange={() => onChange({incrementalLoad: 0})}
-            checked={value.incrementalLoad === 0}>
-            Full Load
-          </Radio>
-          <HelpBlock>
-            All Data in GoodData dataset will be replaced by current Storage table data
-          </HelpBlock>
-
-          <Radio
-            type="radio"
-            title="Incremental"
-            disabled={disabled}
-            onChange={() => onChange({incrementalLoad: 1})}
-            checked={value.incrementalLoad > 0}>
-            Incremental
-          </Radio>
-          <HelpBlock>
-            Data will be appended to the dataset. Only rows created or update in last {' '}
-            <input
-              type="number"
-              value={value.incrementalLoad}
-              onChange={(e) => onChange({incrementalLoad: e.target.value})}
-            />
-            {' '} days will be uploaded
-          </HelpBlock>
+          <Col sm={4} componentClass={ControlLabel}>
+            Load type
+          </Col>
+          <Col sm={8}>
+            <Radio
+              type="radio"
+              title="Full Load"
+              disabled={disabled}
+              onChange={() => onChange({incremental: false, grain: [], changedSince: ''})}
+              checked={!isIncremental}>
+              Full Load
+            </Radio>
+            <HelpBlock>
+              All Data in GoodData dataset will be replaced by current Storage table data.
+            </HelpBlock>
+            <Radio
+              type="radio"
+              title="Incremental"
+              disabled={disabled}
+              onChange={() => onChange({incremental: true})}
+              checked={isIncremental}>
+              Incremental
+            </Radio>
+            <HelpBlock>
+              Data will be appended to the dataset.
+            </HelpBlock>
+          </Col>
         </FormGroup>
-      </form>
+        <FormGroup>
+          <Col componentClass={ControlLabel} sm={4}>
+            Changed In Last
+          </Col>
+          <Col sm={8}>
+            <ChangedSinceInput
+              value={value.changedSince}
+              onChange={(newValue) => this.props.onChange({changedSince: newValue})}
+              disabled={disabled || !isIncremental}
+            />
+          </Col>
+        </FormGroup>
+        <FormGroup>
+          <Col sm={4} componentClass={ControlLabel}>
+            Fact Grain
+          </Col>
+          <Col sm={8}>
+            <Select
+              placeholder="Select at least 2 columns"
+              multi={true}
+              disabled={disabled || !isIncremental || value.hasConnectionPoint}
+              options={value.grainColumns.map(column => ({value: column, label: column}))}
+              value={value.grain}
+              onChange={newColumns => onChange({grain: newColumns.map(column => column.value)})}
+            />
+            <HelpBlock>
+              <a target="_blank" rel="noopener noreferrer" href="https://developer.gooddata.com/article/set-fact-table-grain">
+                Fact grain
+              </a>
+              {' '} columns help to avoid of duplicates records in GoodData dataset without connection point. Specify at least two attribute, reference or date type columns.
+            </HelpBlock>
+          </Col>
+        </FormGroup>
+      </Form>
     );
   }
 });
