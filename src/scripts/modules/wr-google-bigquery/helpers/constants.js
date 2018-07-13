cc7d2fb7af0f7a99d5b67039bbc2b956738c624e
Ondrej Hlavacek <ondrej.hlavacek@keboola.com>
2018-07-13 07:29:33 +0200

    fix(wr-google-bigquery): types order

--- src/scripts/modules/wr-google-bigquery/helpers/constants.js
+++ src/scripts/modules/wr-google-bigquery/helpers/constants.js
@@ -1,10 +1,10 @@
 import keyMirror from 'fbjs/lib/keyMirror';
 
 export const Types = keyMirror({
+  IGNORE: null,
   STRING: null,
   INTEGER: null,
   FLOAT: null,
   BOOLEAN: null,
-  TIMESTAMP: null,
-  IGNORE: null
+  TIMESTAMP: null
 });
