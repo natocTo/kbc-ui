0f9b32fb408f806b2394b1edbb8fecc89120ceff
Ondřej Hlaváček <ondrej.hlavacek@keboola.com>
2018-07-25 16:11:53 +0200

    fix: use const

--- src/scripts/modules/ex-email-attachments/actionsProvisioning.js
+++ src/scripts/modules/ex-email-attachments/actionsProvisioning.js
@@ -55,7 +55,7 @@ export default function(configId) {
   }
 
   function editSave() {
-    let config = store.configData.set('parameters', store.settings);
+    const config = store.configData.set('parameters', store.settings);
     updateLocalState(['isSaving'], true);
     return componentsActions.saveComponentConfigData(COMPONENT_ID, configId, config, 'Update parameters').then(() => {
       removeFromLocalState(['settings']);
