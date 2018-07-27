9f89ea1d081af59dc60cd2fcdb743b65f40dd933
Vladimír Kriška <vlado@keboola.com>
2018-07-27 16:15:42 +0200

    Tune code style a bit

--- src/scripts/modules/jobs/react/pages/jobs-index/QueryRow.jsx
+++ src/scripts/modules/jobs/react/pages/jobs-index/QueryRow.jsx
@@ -4,7 +4,6 @@ import {OverlayTrigger, Popover, Button} from 'react-bootstrap';
 import { Icon } from '@keboola/indigo-ui';
 import SearchRow from '../../../../../react/common/SearchRow';
 
-
 export default React.createClass({
   mixins: [PureRenderMixin],
 
@@ -41,87 +40,94 @@ export default React.createClass({
   },
 
   renderQuickHelp() {
-    return ( <Popover title="Quick help" id="job-search-popover" className="popover-wide" placement="bottom">
-      <h3>Search attributes</h3>
-      <dl>
-        <dt>
-          Job status
-        </dt>
-        <dd>
-          <code>status:success</code>
-        </dd>
-        <dt>
-          User who created the job
-        </dt>
-        <dd>
-          <code>token.description:john.doe@company.com</code>
-        </dd>
-        <dt>
-          Component name
-        </dt>
-        <dd>
-          <code>params.component:keboola.ex-http</code>
-        </dd>
-        <dt>
-          Config ID
-        </dt>
-        <dd>
-          <code>params.config:351711187</code>
-        </dd>
-        <dt>
-          Duration
-        </dt>
-        <dd>
-          <code>durationSeconds:>120</code>
-        </dd>
-        <dt>
-          Time started
-        </dt>
-        <dd>
-          <code>startTime:[2018-06-21 TO 2018-07-01]</code>
-        </dd>
-        <dt>
-          Time finished
-        </dt>
-        <dd>
-          <code>endTime:[2018-06-21 TO 2018-07-01]</code>
-        </dd>
-        <h3>Modifiers and combining queries</h3>
+    return (
+      <Popover
+        title="Quick help"
+        id="job-search-popover"
+        className="popover-wide"
+        placement="bottom"
+      >
+        <h3>Search attributes</h3>
+        <dl>
+          <dt>
+            Job status
+          </dt>
+          <dd>
+            <code>status:success</code>
+          </dd>
+          <dt>
+            User who created the job
+          </dt>
+          <dd>
+            <code>token.description:john.doe@company.com</code>
+          </dd>
+          <dt>
+            Component name
+          </dt>
+          <dd>
+            <code>params.component:keboola.ex-http</code>
+          </dd>
+          <dt>
+            Config ID
+          </dt>
+          <dd>
+            <code>params.config:351711187</code>
+          </dd>
+          <dt>
+            Duration
+          </dt>
+          <dd>
+            <code>durationSeconds:>120</code>
+          </dd>
+          <dt>
+            Time started
+          </dt>
+          <dd>
+            <code>startTime:[2018-06-21 TO 2018-07-01]</code>
+          </dd>
+          <dt>
+            Time finished
+          </dt>
+          <dd>
+            <code>endTime:[2018-06-21 TO 2018-07-01]</code>
+          </dd>
+          <h3>Modifiers and combining queries</h3>
 
-        <dt>
-          Exclude some results
-        </dt>
-        <dd>
-          <code>-status:success</code><br /> Note the minus sign before the query
-        </dd>
+          <dt>
+            Exclude some results
+          </dt>
+          <dd>
+            <code>-status:success</code><br /> Note the minus sign before the query
+          </dd>
 
-        <dt>
-          Combine queries
-        </dt>
-        <dd>
-          <code>+params.component:keboola.ex-http +status:error</code>
-        </dd>
-        <dt>
-          Combine queries with more possible values
-        </dt>
-        <dd>
-          <code>+params.component:(keboola.ex-http OR keboola.wr-google-sheets)</code><br />
-          Jobs from either HTTP or Google Sheets extractor
-        </dd>
-        <dt>
-          Complex query
-        </dt>
-        <dd>
-          <code>+params.component:(keboola.ex-http OR keboola.wr-google-sheets) AND -status:success</code>
-        </dd>
-        <dt>
-          Open ended time query
-        </dt>
-        <dd>
-          <code>endTime:[2018-06-21 TO *]</code>
-        </dd>
-      </dl>
-    </Popover> );
+          <dt>
+            Combine queries
+          </dt>
+          <dd>
+            <code>+params.component:keboola.ex-http +status:error</code>
+          </dd>
+          <dt>
+            Combine queries with more possible values
+          </dt>
+          <dd>
+            <code>+params.component:(keboola.ex-http OR keboola.wr-google-sheets)</code><br />
+            Jobs from either HTTP or Google Sheets extractor
+          </dd>
+          <dt>
+            Complex query
+          </dt>
+          <dd>
+            <code>+params.component:(keboola.ex-http OR keboola.wr-google-sheets) AND -status:success</code>
+          </dd>
+          <dt>
+            Open ended time query
+          </dt>
+          <dd>
+            <code>endTime:[2018-06-21 TO *]</code>
+          </dd>
+        </dl>
+      </Popover>
+    );
   }
 
 });
