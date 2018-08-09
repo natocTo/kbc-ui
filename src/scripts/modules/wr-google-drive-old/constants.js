81ed4177b8d8495b8789dfcf9a1be2598abbcd0d
Vladimír Kriška <vlado@keboola.com>
2018-08-09 13:39:43 +0200

    Convert wr-google-drive-old constants to js

---
+++ src/scripts/modules/wr-google-drive-old/constants.js
@@ -0,0 +1,19 @@
+import keyMirror from 'fbjs/lib/keyMirror';
+
+export const ActionTypes = keyMirror({
+  WR_GDRIVE_LOAD_FILES_SUCCESS: null,
+  WR_GDRIVE_API_ERROR: null,
+
+  WR_GDRIVE_LOAD_GOOGLEINFO_SUCCESS: null,
+  WR_GDRIVE_LOAD_GOOGLEINFO_START: null,
+
+  WR_GDRIVE_SET_EDITING: null,
+
+  WR_GDRIVE_SAVEFILE_START: null,
+
+  WR_GDRIVE_SAVEFILE_SUCCESS: null,
+
+  WR_GDRIVE_DELETE_ROW_START: null,
+  WR_GDRIVE_DELETE_ROW_DELETE: null,
+  WR_GDRIVE_LOAD_ACCOUNT_SUCCESS: null
+});
