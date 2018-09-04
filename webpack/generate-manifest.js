bf6f051f507307258f8707cfa248ff94965271c7
Jakub Kotek <kotek@zerostudio.cz>
2018-09-04 22:21:51 +0200

    Fix codestyle of some files

--- webpack/generate-manifest.js
+++ webpack/generate-manifest.js
@@ -1,24 +1,20 @@
 var fs = require('fs');
 
 var revision = process.env.KBC_REVISION;
-var s3basePath = "https://assets-cdn.keboola.com/kbc-ui/" + revision + "/";
+var s3basePath = 'https://assets-cdn.keboola.com/kbc-ui/' + revision + '/';
 
 var manifest = {
-    "name": "kbc",
-    "version": revision,
-    basePath: s3basePath,
-    "scripts": [
-        s3basePath + 'bundle.min.js'
-    ],
-    "styles": [
-        s3basePath + 'bundle.min.css'
-    ]
+  name: 'kbc',
+  version: revision,
+  basePath: s3basePath,
+  scripts: [s3basePath + 'bundle.min.js'],
+  styles: [s3basePath + 'bundle.min.css']
 };
 
 console.log(manifest);
 
-fs.writeFile("./dist/" + revision + "/manifest.json", JSON.stringify(manifest), function(err) {
-   if (err) {
-       throw err;
-   }
+fs.writeFile('./dist/' + revision + '/manifest.json', JSON.stringify(manifest), function(err) {
+  if (err) {
+    throw err;
+  }
 });
--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,3 +1,3 @@
-module.exports = require("./make-config")({
-	isDevelopment: false
+module.exports = require('./make-config')({
+  isDevelopment: false
 });
